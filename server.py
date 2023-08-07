538058f4ca34ecd22a2449fb2e63892d998c791f
Pedro Porto Buarque de Gusmao <pedropgusmao@gmail.com>
2023-08-07 20:30:58 +0100

    Adding num_rounds in server

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ def main(cfg: DictConfig) -> None:
     # Start Flower server
     fl.server.start_server(
         server_address="0.0.0.0:8080",
-        config=fl.server.ServerConfig(num_rounds=3),
+        config=fl.server.ServerConfig(num_rounds=cfg.num_rounds),
         strategy=strategy,
     )
 
