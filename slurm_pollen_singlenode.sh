1b55d2c6f89e973e25f3bdc58f0749ef3cc1f006
relogu <lollonasi97@gmail.com>
2023-08-23 11:01:22 +0100

    update with the new script

--- slurm_pollen_singlenode.sh
+++ slurm_pollen_singlenode.sh
@@ -11,6 +11,17 @@ timestamp=$(date +%Y-%m-%d_%H%M%S)
 source /nfs-share/ls985/anaconda3/bin/activate ray111-test
 cd /nfs-share/ls985/pollen_worker
 
-python server.py num_rounds=1 & # >> server_$timestamp.out 2>&1 &
-sleep 10
-python node_manager.py # >> node_manager_$timestamp.out 2>&1
+# Clean the shared memory objects
+python clean_memory.py
+
+# Launch the server
+python server_with+pollen.py num_rounds=100 & # >> server_$timestamp.out 2>&1 &
+# Waiting for the server to start
+sleep 15
+# Launch the node manager
+python pure_sh_node_manager.py # >> node_manager_$timestamp.out 2>&1
+
+# Clean the shared memory objects
+python clean_memory.py
+
+# srun -w mauao -c 11 --gres=gpu:1 --partition=interactive bash slurm_pollen_singlenode.sh
