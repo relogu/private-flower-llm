5c8484edae52847f0207939d2d89d7e08bb4aa18
relogu <lollonasi97@gmail.com>
2023-08-23 11:01:46 +0100

    generalised to fixed constants for shared memory

--- clean_memory.py
+++ clean_memory.py
@@ -1,20 +1,25 @@
 from multiprocessing.shared_memory import SharedMemory
-import sys
+
+from pure_sh_node_manager import POLLEN_CONFIG_SHM, POLLEN_PARAMETERS_SHM, POLLEN_WORKER_SHM
+
 for i in range(10):
     try:
-        name = f'pollen_worker_{i}'
+        # name = f'pollen_worker_{i}'
+        name = POLLEN_WORKER_SHM+f'{i}'
         shm = SharedMemory(name=name, create=False)
         shm.unlink() 
     except:
         pass
 try:
-    name = f'pollen_config_shm'
+    # name = f'pollen_config_shm'
+    name = POLLEN_CONFIG_SHM
     shm = SharedMemory(name=name, create=False)
     shm.unlink() 
 except:
     pass
 try:
-    name = f'pollen_parameters'
+    # name = f'pollen_parameters'
+    name = POLLEN_PARAMETERS_SHM
     shm = SharedMemory(name=name, create=False)
     shm.unlink() 
 except:
