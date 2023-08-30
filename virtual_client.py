62b78ead25678767c53245ca4a82d50559d45a11
relogu <lollonasi97@gmail.com>
2023-08-30 15:34:44 +0100

    optimisations and clean up

--- virtual_client.py
+++ virtual_client.py
@@ -1,15 +1,18 @@
 from collections import OrderedDict
 from logging import INFO
-from typing import Dict
+from typing import Callable,Dict
 
 import flwr as fl
 import torch
+from flwr.client import NumPyClient
 from flwr.common.logger import log
 from flwr.common.typing import Config, NDArrays, Scalar
 from torch.nn import Module
+from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
 from models.training_loops import get_input_shapes, get_training_loop
+from datasets.nlp_util import get_collate_fn
 from pollen_utils import get_client_ds, get_model, get_optimizer
 
 
@@ -19,17 +22,13 @@ class VirtualClient(fl.client.NumPyClient):
         *,
         name: str,
         cid: int,
-        device: torch.device,
-        n_workers: int,
     ):
         self.name = name
         self.cid = cid
-        self.device = device
-        self.n_workers = n_workers
         # log(INFO, f'VirtualClient.__init__ :: cid {self.cid}')
 
     def __repr__(self) -> str:
-        return f"VirtualClient(name={self.name}, cid={self.cid}, device={self.device}, n_workers={self.n_workers})"
+        return f"VirtualClient(name={self.name}, cid={self.cid})"
 
     def get_properties(self, config: Config) -> Dict[str, Scalar]:
         return {}
@@ -37,6 +36,7 @@ class VirtualClient(fl.client.NumPyClient):
     def get_parameters(self, config, net=None, device="cpu", to_numpy=True):
         if net is None:
             net = get_model(name=self.name)
+        net.eval()
         if device == "cpu" and to_numpy:
             tmp = [
                 val.detach().to(device).numpy()
@@ -57,12 +57,13 @@ class VirtualClient(fl.client.NumPyClient):
             ]
         return tmp
 
-    def set_parameters(self, parameters: NDArrays, net: Module = None):
+    def set_parameters(self, parameters: NDArrays, net: Module = None, device="cpu"):
         if net is None:
             net = get_model(name=self.name)
+        net.eval()
         keys = [k for k in net.state_dict().keys() if "bn" not in k]
         params_dict = zip(keys, parameters)
-        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        state_dict = OrderedDict({k: torch.tensor(v, device=device) for k, v in params_dict})
         net.load_state_dict(state_dict, strict=False)
         return net
 
@@ -71,24 +72,30 @@ class VirtualClient(fl.client.NumPyClient):
         net: Module,
         batch_size: int,
         device: torch.device,
+        optimizer: Optimizer,
+        criterion: Module,
         epochs: int,
         **kwargs,
     ):
         """Train the model on the training set of single client."""
-        # log(INFO, f'VirtualClient._train_loop :: with cid {self.cid}')
-        # Get the optimiser
-        optimizer = get_optimizer(name=self.name, model=net)
-        # Get the criterion
-        criterion = torch.nn.CrossEntropyLoss(reduction="none").to(device=device)
+        log(INFO, f'VirtualClient._train_loop :: FAKE with cid {self.cid}')
         # Load the fake data directly into the VRAM
         input_shape = get_input_shapes(name=self.name)
-        fake_data = torch.zeros(((batch_size,) + input_shape), device=device)
-        fake_targets = torch.zeros((batch_size,), device=device).long()
+        if self.name == "reddit":
+            fake_data = torch.zeros(((batch_size,) + input_shape), device=device).long()
+            fake_targets = torch.zeros((batch_size,) + input_shape, device=device).long()
+        else:
+            fake_data = torch.zeros(((batch_size,) + input_shape), device=device)
+            fake_targets = torch.zeros((batch_size,), device=device).long()
         for _ in range(epochs):
             # ========= Define the forward pass ==============
-            output = net(fake_data)
-            loss = criterion(output, fake_targets)
-            loss = loss.mean()
+            if self.name == "reddit":
+                output = net(fake_data, labels=fake_targets)
+                loss = output[0]
+            else:
+                output = net(fake_data)
+                loss = criterion(output, fake_targets)
+                loss = loss.mean()
 
             # ========= Define the backward pass ==============
             optimizer.zero_grad()
@@ -99,9 +106,8 @@ class VirtualClient(fl.client.NumPyClient):
 
     def fit(self, parameters: NDArrays, config: Dict[str, Scalar]):
         # log(INFO, f'VirtualClient.fit :: {config}')
-        # TODO: Load client's dataset
-        ds, tokenizer = get_client_ds(name=self.name, cid=self.cid)
-        # ds, tokenizer = None, None
+        # Load client's dataset
+        ds, tokenizer = get_client_ds(name=self.name, cid=self.cid) if not config["is_fake"] else (None, None)
         # Instantiate the trainloader
         trainloader = (
             DataLoader(
@@ -109,16 +115,16 @@ class VirtualClient(fl.client.NumPyClient):
                 dataset=ds,
                 batch_size=config["batch_size"],
                 shuffle=False,
-                num_workers=self.n_workers,
+                num_workers=config["n_workers"],
                 drop_last=True,
                 pin_memory=True,  # copy Tensors into CUDA pinned memory before returning them
                 pin_memory_device=str(
-                    self.device
+                    config["device"]
                 ),  # the device to be used for pinning the memory
                 # NOTE: Default arguments
                 sampler=None,  # how to draw sample from the dataset
                 batch_sampler=None,  # like the above but for batches
-                collate_fn=None,  # builds batches from samples
+                collate_fn=get_collate_fn(tokenizer=tokenizer) if tokenizer is not None else None,  # builds batches from samples
                 timeout=0,  # if positive, the timeout value for collecting a batch from workers
                 worker_init_fn=None,  # init function for worker processes
                 multiprocessing_context=None,
@@ -126,40 +132,31 @@ class VirtualClient(fl.client.NumPyClient):
                 prefetch_factor=2,  # number of batches loaded in advance by each worker
                 persistent_workers=False,  # if True, the data loader will not shutdown the worker processes after a dataset has been consumed once. This allows to maintain the workers
             )
-            if ds is not None
+            if not config["is_fake"]
             else None
         )
         # Get the number of samples
         n_samples = (
-            int(config["batch_size"] * config["epochs"]) if ds is None else len(ds)
+            int(config["batch_size"] * config["local_epochs"]) if ds is None else len(ds)
         )
         # Initialize the model and set its parameters
         net = self.set_parameters(parameters=parameters)
-        net.to(device=self.device)
+        net.to(device=config["device"])
         net.train()
         # Train the model
-        # # Fake thing
-        # net = self._train_loop(
-        #     trainloader=trainloader,
-        #     net=net,
-        #     batch_size=config["batch_size"],
-        #     device=self.device,
-        #     epochs=config["epochs"],
-        #     tokenizer=tokenizer,
-        # )
-        # Real thing
-        training_loop = get_training_loop(name=self.name)
+        self._train_loop = get_training_loop(name=self.name) if not config["is_fake"] else self._train_loop
         optimizer = get_optimizer(name=self.name, model=net)
-        criterion = torch.nn.CrossEntropyLoss(reduction="none").to(device=self.device)
-        net = training_loop(
+        criterion = torch.nn.CrossEntropyLoss(reduction="none").to(device=config["device"])
+        # net = training_loop(
+        net = self._train_loop(
             trainloader=trainloader,
             net=net,
-            batch_size=config["batch_size"],
-            device=self.device,
-            epochs=config["epochs"],
+            device=config["device"],
+            epochs=config["local_epochs"],
             tokenizer=tokenizer,
             optimizer=optimizer,
             criterion=criterion,
+            batch_size=config["batch_size"],
         )
         return self.get_parameters(config={}, net=net), n_samples, {}
 
@@ -171,9 +168,17 @@ class VirtualClient(fl.client.NumPyClient):
         return 0.0, 0, {"local_accuracy": 0.0}
 
 
+def gen_client_fn(name: str = "openimage", **kwargs) -> Callable[[int], NumPyClient]:
+    def client_fn(client_id: int) -> NumPyClient:
+        client = VirtualClient(name=name, cid=client_id)
+        return client
+
+    return client_fn
+
+
 if __name__ == "__main__":
     log(INFO, "VirtualClient.__main__ :: testing VirtualClient")
-    from pollen.utils import get_device
+    from pollen_utils import get_device
 
     client = VirtualClient(
         name="openimage",
