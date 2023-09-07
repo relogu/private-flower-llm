6bb9c5480ba32be04064d85176b981146c27cc07
relogu <lollonasi97@gmail.com>
2023-09-07 15:31:52 +0100

    handled warning about tensor creation

--- datasets/nlp_util.py
+++ datasets/nlp_util.py
@@ -276,39 +276,24 @@ def mask_tokens(
     if tokenizer._pad_token is not None:
         padding_mask = labels.eq(tokenizer.pad_token_id)
         probability_matrix.masked_fill_(padding_mask, value=0.0)
-    masked_indices = (
-        torch.tensor(torch.bernoulli(probability_matrix), dtype=torch.bool)
-        .detach()
-        .to(device=device)
-    )
+    masked_indices = (torch.bernoulli(probability_matrix) == 1).to(device=device)
     labels[~masked_indices] = -100  # We only compute loss on masked tokens
 
-    # FIXME: the following warning is printed in the logs
-    # `$FEDSCALE_HOME/fedscale/dataloaders/nlp.py:195: UserWarning: To copy construct from a tensor,
-    # it is recommended to use sourceTensor.clone().detach() or sourceTensor.clone().detach().requires_grad_(True),
-    # rather than torch.tensor(sourceTensor).labels.shape, 0.8)), dtype=torch.bool, device=device) & masked_indices`
     # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
-    indices_replaced = (
-        torch.tensor(
-            torch.bernoulli(torch.full(labels.shape, 0.8)),
-            dtype=torch.bool,
-            device=device,
-        )
-        & masked_indices
-    )
+    indices_replaced = (torch.bernoulli(torch.full(labels.shape, 0.8)) == 1).to(
+        device=device
+    ) & masked_indices
     inputs[indices_replaced] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
 
     # 10% of the time, we replace masked input tokens with random word
     indices_random = (
-        torch.tensor(
-            torch.bernoulli(torch.full(labels.shape, 0.5)),
-            dtype=torch.bool,
-            device=device,
-        )
+        (torch.bernoulli(torch.full(labels.shape, 0.5)) == 1).to(device=device)
         & masked_indices
         & ~indices_replaced
     )
-    random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long)
+    random_words = torch.randint(
+        len(tokenizer), labels.shape, dtype=torch.long, device=device
+    )
     bool_indices_random = indices_random
     inputs[bool_indices_random] = random_words[bool_indices_random]
 
