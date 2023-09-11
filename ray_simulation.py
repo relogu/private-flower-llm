26adf907f3edae916d08f1751829908db99dbe48
relogu <lollonasi97@gmail.com>
2023-09-11 15:26:10 +0100

    update compatibility with latest changes

--- ray_simulation.py
+++ ray_simulation.py
@@ -9,6 +9,7 @@ import flwr as fl
 import hydra
 import nvsmi
 import torch
+import transformers
 from flwr.client import ClientLike
 from flwr.common import ndarrays_to_parameters
 from flwr.common.logger import log
@@ -20,40 +21,40 @@ from rs_fedavg import FedAvgReproducibleSampling
 from utils import weighted_average
 from virtual_client import VirtualClient
 
+transformers.logging.set_verbosity_error()
+
 warnings.filterwarnings("ignore", category=UserWarning)
 
 parser = argparse.ArgumentParser(description="Flower Simulation with PyTorch")
 
 parser.add_argument("--num_client_cpus", type=int, default=1)
 parser.add_argument("--num_rounds", type=int, default=1)
-parser.add_argument("--name", type=str, default='openimage')
+parser.add_argument("--name", type=str, default="openimage")
 
 
-def get_n_worker_gpu_type(name: str = 'openimage'):
-    if name == 'reddit':
+def get_n_worker_gpu_type(name: str = "openimage"):
+    # NOTE: These numbers are compatible with the last version of Pollen
+    if name == "reddit":
         return {
-            'NVIDIA A40': 1,
-            'Tesla V100-PCIE-32GB': 1,
-            'NVIDIA GeForce RTX 2080 Ti': 1,
+            "NVIDIA A40": 14,
+            "NVIDIA GeForce RTX 2080 Ti": 3,
         }
-    elif name == 'shakespeare' or name == 'shakespeare_memory':
+    elif name == "shakespeare" or name == "shakespeare_memory":
         return {
-            'NVIDIA A40': 10,
-            'Tesla V100-PCIE-32GB': 10,
-            'NVIDIA GeForce RTX 2080 Ti': 10,
+            "NVIDIA A40": 36,
+            "NVIDIA GeForce RTX 2080 Ti": 11,
         }
-    elif name == 'google_speech':
+    elif name == "google_speech":
         return {
-            'NVIDIA A40': 17,
-            'Tesla V100-PCIE-32GB': 19,
-            'NVIDIA GeForce RTX 2080 Ti': 7,
+            "NVIDIA A40": 22,
+            "NVIDIA GeForce RTX 2080 Ti": 7,
         }
-    elif name == 'openimage':
+    elif name == "openimage":
         return {
-            'NVIDIA A40': 13,
-            'Tesla V100-PCIE-32GB': 13,
-            'NVIDIA GeForce RTX 2080 Ti': 4,
+            "NVIDIA A40": 15,
+            "NVIDIA GeForce RTX 2080 Ti": 4,
         }
+
 
 # Define strategy
 @hydra.main(config_path="conf/", config_name="base", version_base=None)
@@ -64,21 +65,24 @@ def main(cfg: DictConfig) -> None:
     )
 
     # number of dataset partions (= number of total clients)
-    pool_size = 10 if 'shakespeare' in cfg.task.name else 100
+    pool_size = 10 if "shakespeare" in cfg.task.name else 100
     num_available_gpus = torch.cuda.device_count()
     gpu_name = [gpu.name for gpu in nvsmi.get_gpus()]
-    n_workers = min(pool_size, num_available_gpus *
-                    get_n_worker_gpu_type(cfg.task.name)[gpu_name[0]])
+    n_workers = min(
+        pool_size,
+        num_available_gpus * get_n_worker_gpu_type(cfg.task.name)[gpu_name[0]],
+    )
     print(f"n_workers: {n_workers}")
 
     client_resources = {
-        "num_gpus": num_available_gpus/n_workers,
+        "num_gpus": num_available_gpus / n_workers,
     }
 
     # Get the list of cids
     cid_samples_dict = get_clients_population_dict(
         name=cfg.task.name,
-        batch_size=cfg.task.batch_size,)
+        batch_size=cfg.task.batch_size,
+    )
     n_total_clients = len(cid_samples_dict)
     n_clients_per_round = cfg.task.n_clients_per_round
 
@@ -106,8 +110,8 @@ def main(cfg: DictConfig) -> None:
     log(INFO, f"Fraction fit is: {strategy.fraction_fit}")
 
     # (optional) specify Ray config
-    print(f'Using {int(n_workers)} CPUs overall')
-    print(f'Having affinity {os.sched_getaffinity(0)}')
+    print(f"Using {int(n_workers)} CPUs overall")
+    print(f"Having affinity {os.sched_getaffinity(0)}")
     ray_init_args = {
         # "address": cfg.ray_address,
         "include_dashboard": False,
@@ -116,8 +120,10 @@ def main(cfg: DictConfig) -> None:
         "log_to_driver": True,
         "_system_config": {
             "object_spilling_config": json.dumps(
-                {"type": "filesystem", "params": {
-                    "directory_path": "/hdd1/ray/ray_spilled_objects/"}},
+                {
+                    "type": "filesystem",
+                    "params": {"directory_path": "/hdd1/ray/ray_spilled_objects/"},
+                },
             )
         },
     }
