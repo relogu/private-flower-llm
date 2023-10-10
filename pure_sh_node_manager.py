9d58d475f5a6fd943f2bd859c719da0e1db18215
relogu <lollonasi97@gmail.com>
2023-10-10 09:31:03 +0100

    imports

--- pure_sh_node_manager.py
+++ pure_sh_node_manager.py
@@ -3,11 +3,7 @@ import os
 import pickle
 import time
 from collections import defaultdict
-<<<<<<< HEAD
-from logging import DEBUG, INFO, ERROR
-=======
 from logging import DEBUG, ERROR
->>>>>>> main
 from multiprocessing import resource_tracker
 from multiprocessing.queues import Queue as QueueType
 from multiprocessing.shared_memory import SharedMemory
