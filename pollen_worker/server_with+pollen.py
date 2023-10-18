216932ee483b416b5db7f82bd86d90307825156f
relogu <lollonasi97@gmail.com>
2023-10-18 17:36:54 +0100

    fix small bug

--- pollen_worker/server_with+pollen.py
+++ pollen_worker/server_with+pollen.py
@@ -73,9 +73,9 @@ def main(cfg: DictConfig) -> None:
     strategy = instantiate(
         cfg.task.strategy,
         saving_path=Path(hydra_cfg["runtime"]["output_dir"]),  # type: ignore[index]
-        min_fit_clients=2,
+        min_fit_clients=n_clients_per_round,
         fraction_evaluate=0.0,
-        fraction_fit=n_clients_per_round / n_total_clients,
+        fraction_fit=(1.0 / n_total_clients),
         on_fit_config_fn=on_fit_config_fn,
         initial_parameters=ndarrays_to_parameters(
             get_client_fn(cid=0).get_parameters(config={}, net=None)  # type: ignore
