c5b2d311dfd389e0debd71f28e73d8f45f8dd2fa
relogu <lollonasi97@gmail.com>
2023-12-20 15:50:51 +0000

    fix mistakes

--- llm_slurm/install_hpc_env.sh
+++ llm_slurm/install_hpc_env.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 #! Add modules from scratch to be sure everything works
-. /etc/profile.d/modules.sh                # Leave this line (enables the module command)
-module purge                               # Removes all modules still loaded
-module load rhel8/default-amp              # REQUIRED - loads the basic environment
-#! Remove old cuda
-module unload cuda/11.4
-module unload cuda/11.4.0/gcc-9.4.0-3hnxhjt
+#! Enable the module command
+. /etc/profile.d/modules.sh
+#! Remove all modules still loaded
+module purge
+#! Load base modules
+module load singularity/current
+module load rhel8/slurm
+module load dot
+module load rhel8/global
+module -s load openmpi/4.1.1/gcc-9.4.0-epagguv # This might be unable to locate
 #! Load cuda 12.1
 module load cuda/12.1
 module load cudnn/8.9_cuda-12.1
 #! Load additional modules
 module load ceuadmin/gettext/0.20
 module load vgl/2.5.1/64
-module load intel-oneapi-compilers/2022.1.0/gcc/b6zld2mz # This might/should fail
-module load intel-oneapi-mpi/2021.6.0/intel/guxuvcpm # This might/should fail
-module load rhel8/default-icl
 #! Check the output of `nvcc -V`
 nvcc -V
-#! Check Python version
-python --version
 #! Entering the project folder
 cd $HOME/projects/pollen_worker
 #! Install Poetry environment
-poetry install
+poetry install -q
+#! Install `flash-attn`
+poetry run pip install flash-attn==2.3.2 --no-build-isolation
+#! Install `xentropy-cuda-lib`
+poetry run pip install xentropy-cuda-lib@git+https://github.com/HazyResearch/flash-attention.git@v2.3.2#subdirectory=csrc/xentropy
+#! Check Python version
+python --version
