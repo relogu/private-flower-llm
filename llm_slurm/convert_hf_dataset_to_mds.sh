7958ec64f0b48a3039c2d03c2eab88e36406f509
relogu <lollonasi97@gmail.com>
2023-12-22 14:34:03 +0000

    improved usage

--- llm_slurm/convert_hf_dataset_to_mds.sh
+++ llm_slurm/convert_hf_dataset_to_mds.sh
@@ -1,7 +1,60 @@
 #!/bin/bash
+#! Check if there's an input argument
+if [[ $1 = "--help" ]] || [[ $1 = "-h" ]]; then
+    echo "Usage: bash convert_hf_dataset_to_mds.sh <split> <n_clients> <dataset> <dataset_subset> <data_root>."
+    echo -e "\t<split>: 'small' or 'full'. Default: 'small'"
+    echo -e "\t<n_clients>: integer. Default: 10"
+    echo -e "\t<dataset>: 'c4' or 'pile'. Default: 'c4'"
+    echo -e "\t<dataset_subset>: 'en' or 'all'. Default: 'en'"
+    echo -e "\t<data_root>: path. Default: '/local/scratch'"
+    echo -e "\tExample: bash convert_hf_dataset_to_mds.sh small 10 c4 en"
+    exit 1
+fi
+#! Set the variables
+SPLIT="small"
+N_CLIENTS=10
+DATASET="c4"
+DATASET_SUBSET="en"
+MOSAICML_DATA_ROOT="/local/scratch"
+if [[ $# -eq 0 ]]; then
+    echo "Using default values for all input arguments."
+elif [[ $# -eq 1 ]]; then
+    SPLIT=$1
+elif [[ $# -eq 2 ]]; then
+    SPLIT=$1
+    N_CLIENTS=$2
+elif [[ $# -eq 3 ]]; then
+    SPLIT=$1
+    N_CLIENTS=$2
+    DATASET=$3
+elif [[ $# -eq 4 ]]; then
+    SPLIT=$1
+    N_CLIENTS=$2
+    DATASET=$3
+    DATASET_SUBSET=$4
+elif [[ $# -eq 5 ]]; then
+    SPLIT=$1
+    N_CLIENTS=$2
+    DATASET=$3
+    DATASET_SUBSET=$4
+    MOSAICML_DATA_ROOT=$5
+else
+    echo "Invalid number of input arguments. Try 'bash convert_hf_dataset_to_mds.sh --help/-h' for more information."
+    exit 1
+fi
+mkdir -p $MOSAICML_DATA_ROOT
+if [[ $SPLIT == "small" ]]; then
+    SPLIT_NAME="train_small val_small"
+elif [[ $SPLIT == "full" ]]; then
+    SPLIT_NAME="train val"
+else
+    echo "Invalid split. Try 'bash convert_hf_dataset_to_mds.sh --help/-h' for more information."
+    exit 1
+fi
 #! Moving to the project folder
 cd $HOME/projects/pollen_worker
-if ! [[ $(hostname) == 'mauao' ]] && ! [[ $(hostname) == *'fluidstack'* ]]; then
+#! Preparing environment
+if [[ $(hostname) == *'gpu-q'* ]]; then
     echo "Assuming the script is executing in the CSD3."
     #! Executing the environment preparation script
     #! NOTE: Must use "." to execute, "sh" doesn't work
@@ -10,24 +63,6 @@ fi
 #! Activate Poetry environment
 POETRY_ENV_PATH=$(poetry env info --path)
 . $POETRY_ENV_PATH/bin/activate
-#! Set the variables
-DATASET="c4"
-DATASET_SUBSET="en" # Can also be "all"
-N_CLIENTS=10
-#! Check whether the MOSAICML_DATA_ROOT has been set or not
-if [ -z "${MOSAICML_DATA_ROOT}" ]; then
-    echo "MOSAICML_DATA_ROOT is not set. Exiting..."
-    exit 1
-else
-    echo "MOSAICML_DATA_ROOT is set to '${MOSAICML_DATA_ROOT}'"
-fi
-#! Check whether the MOSAICML_DATA_ROOT directory exists or not
-if [ -d "$MOSAICML_DATA_ROOT" ]; then
-    echo "MOSAICML_DATA_ROOT directory exists"
-else
-    echo "MOSAICML_DATA_ROOT directory does not exist. Creating..."
-    mkdir -p $MOSAICML_DATA_ROOT
-fi
 #! Set the data root
 DATA_ROOT="$MOSAICML_DATA_ROOT/fed_$DATASET/c$N_CLIENTS"
 echo "Creating the partition data root directory: $DATA_ROOT"
@@ -39,12 +74,11 @@ else
     export NUM_CPUS=$SLURM_CPUS_PER_TASK
 fi
 echo "Number of CPU cores available: $NUM_CPUS"
-
 #! Execute the command
 poetry run python -m pollen_worker.dataset.convert_dataset_hf \
     --dataset $DATASET \
     --data_subset $DATASET_SUBSET \
-    --splits train_small val_small \
+    --splits $SPLIT_NAME \
     --out_root $DATA_ROOT \
     --compression zstd \
     --concat_tokens 2048 \
