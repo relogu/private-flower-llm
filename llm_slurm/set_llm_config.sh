2821f67a06fb648cb4891ce97529b6960c77bde7
Iacob Alexandru-Andrei <iacobalexandruandrei@gmail.com>
2023-12-25 23:32:18 +0000

    Add temporary values to set_llm_config

--- llm_slurm/set_llm_config.sh
+++ llm_slurm/set_llm_config.sh
@@ -10,6 +10,7 @@ GPU_TYPE=$(nvidia-smi -L)
 if [[ $GPU_TYPE == *'A40'* ]]; then
     echo "Assuming we are running on A40s (Mauao)."
     LLM_CONFIG_MPT_SMALL_CPU="llm_config=mpt-small-cpu llm_config.model.init_device=meta llm_config.model.loss_fn=fused_crossentropy llm_config.model.attn_config.attn_impl=triton llm_config.precision=amp_bf16 llm_config.device_train_microbatch_size=256 llm_config.eval_subset_num_batches=-1 llm_config.device_eval_batch_size=128" # The model is so small that the batch sizes really don't matter
+    LLM_CONFIG_MPT_15M="llm_config=mpt-15m llm_config.device_train_microbatch_size=64 llm_config.device_eval_batch_size=50"
     LLM_CONFIG_MPT_125M="llm_config=mpt-125m llm_config.device_train_microbatch_size=20 llm_config.device_eval_batch_size=50"
     LLM_CONFIG_MPT_350M="llm_config=mpt-350m llm_config.device_train_microbatch_size=18 llm_config.device_eval_batch_size=40"
     LLM_CONFIG_MPT_760M="llm_config=mpt-760m llm_config.device_train_microbatch_size=4 llm_config.device_eval_batch_size=35 llm_config.model.attn_config.attn_impl=torch" # Cannot use Triton here
@@ -21,6 +22,7 @@ elif [[ $GPU_TYPE == *'A100'* ]]; then
     echo "Assuming we are running on A100-SXM4-80GB-equipped machines (CSD3)."
     #! NOTE: We didn't investigate the performance at inference
     LLM_CONFIG_MPT_SMALL_CPU="llm_config=mpt-small-cpu llm_config.model.init_device=meta llm_config.model.loss_fn=fused_crossentropy llm_config.model.attn_config.attn_impl=triton llm_config.precision=amp_bf16 llm_config.device_train_microbatch_size=512 llm_config.eval_subset_num_batches=-1" # The model is so small that the batch sizes really don't matter
+    LLM_CONFIG_MPT_15M="llm_config=mpt-15m llm_config.device_train_microbatch_size=128 llm_config.device_eval_batch_size=50"
     LLM_CONFIG_MPT_125M="llm_config=mpt-125m llm_config.device_train_microbatch_size=20" # This has issues (we can't scale up the microbatch size even though it seems possible)
     LLM_CONFIG_MPT_350M="llm_config=mpt-350m llm_config.device_train_microbatch_size=20" # This has issues (we can't scale up the microbatch size even though it seems possible)
     LLM_CONFIG_MPT_760M="llm_config=mpt-760m llm_config.device_train_microbatch_size=20"
@@ -32,6 +34,7 @@ elif [[ $GPU_TYPE == *'L40'* ]]; then
     echo "Assuming we are running on L40s-equipped machines (Fluidstack)."
     #! NOTE: We didn't investigate the performance at inference
     LLM_CONFIG_MPT_SMALL_CPU="llm_config=mpt-small-cpu llm_config.model.init_device=meta llm_config.model.loss_fn=fused_crossentropy llm_config.model.attn_config.attn_impl=triton llm_config.precision=amp_bf16 llm_config.device_train_microbatch_size=256 llm_config.eval_subset_num_batches=-1 llm_config.device_eval_batch_size=128" # The model is so small that the batch sizes really don't matter
+    LLM_CONFIG_MPT_15M="llm_config=mpt-15m llm_config.device_train_microbatch_size=128 llm_config.device_eval_batch_size=50"
     LLM_CONFIG_MPT_125M="llm_config=mpt-125m llm_config.device_train_microbatch_size=50 llm_config.device_eval_batch_size=50" # This has issues (we can't scale up the microbatch size even though it seems possible)
     LLM_CONFIG_MPT_350M="llm_config=mpt-350m llm_config.device_train_microbatch_size=15 llm_config.device_eval_batch_size=40"
     LLM_CONFIG_MPT_760M="llm_config=mpt-760m llm_config.device_train_microbatch_size=3 llm_config.device_eval_batch_size=35 llm_config.model.attn_config.attn_impl=torch" # Same as A40s: cannot use Triton here. Weirdly, we had to downgrade the microbatch size
@@ -45,6 +48,8 @@ fi
 #! Set the run configuration
 if [[ "$1" == "small" ]]; then
     export LLM_CONFIG=$LLM_CONFIG_MPT_SMALL_CPU
+elif [[ "$1" == "15M" ]]; then
+    export LLM_CONFIG=$LLM_CONFIG_MPT_15M
 elif [[ "$1" == "125M" ]]; then
     export LLM_CONFIG=$LLM_CONFIG_MPT_125M
 elif [[ "$1" == "350M" ]]; then
@@ -61,7 +66,7 @@ elif [[ "$1" == "70B" ]]; then
     export LLM_CONFIG=$LLM_CONFIG_MPT_70B
 else
     echo "Invalid input argument: $1"
-    echo "Valid input arguments are: small, 125M, 350M, 760M, 1B, 3B, 7B, 70B"
+    echo "Valid input arguments are: small, 15M, 125M, 350M, 760M, 1B, 3B, 7B, 70B"
     exit 1
 fi
 
