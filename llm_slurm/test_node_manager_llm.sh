0d4554b78912095bce64f102a5fcd9a8852a967b
relogu <lollonasi97@gmail.com>
2023-12-27 16:52:53 +0000

    add testing options

--- llm_slurm/test_node_manager_llm.sh
+++ llm_slurm/test_node_manager_llm.sh
@@ -46,4 +46,4 @@ mkdir -p $SAVE_PATH
 TESTING_OPTIONS="llm_config.max_duration=100ba"
 
 #! Test NodeManager
-HYDRA_FULL_ERROR=1 poetry run python -m pollen_worker.node_manager.node_manager $LLM_CONFIG $LLM_OPTIONS $DATA_CONFIG is_test=true hydra/job_logging=none hydra/hydra_logging=none 2>&1 | tee $SAVE_PATH/node_manager.log 
+HYDRA_FULL_ERROR=1 poetry run python -m pollen_worker.node_manager.node_manager $LLM_CONFIG $LLM_OPTIONS $DATA_CONFIG $TESTING_OPTIONS is_test=true hydra/job_logging=none hydra/hydra_logging=none 2>&1 | tee $SAVE_PATH/node_manager.log 
