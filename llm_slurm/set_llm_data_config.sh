87b1296cd6731d33770fd128c3c62adc7593326a
relogu <lollonasi97@gmail.com>
2023-12-28 09:05:05 +0000

    chnaged tmp dir

--- llm_slurm/set_llm_data_config.sh
+++ llm_slurm/set_llm_data_config.sh
@@ -6,7 +6,8 @@ if [[ $(hostname) == *'gpu-q'* ]]; then
 else
     echo "Assuming the script is executing NOT in the CSD3."
     # export DATA_TMP_DIR="$HOME/tmp"
-    export DATA_TMP_DIR="/tmp"
+    export DATA_TMP_DIR="/home/$USER/tmp"
+    # export DATA_TMP_DIR="/tmp"
 fi
 mkdir -p $DATA_TMP_DIR
 #! Check if the external var has been set
