bd6943e7843b1aae6b03ad55cfb96befcfa4931d
relogu <lollonasi97@gmail.com>
2024-01-06 16:09:17 +0000

    bug fixed and verbosity

--- llm_slurm/install_env.sh
+++ llm_slurm/install_env.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 #! Moving to the project folder
 cd $HOME/projects/pollen_worker
-#! Install Poetry environment
-if ! [[ $(poetry check --lock) ]]; then
-    echo "Installing Poetry environment..."
-    poetry install -q
-else
-    echo "Poetry environment already installed."
-fi
 #! Activate Poetry environment
 POETRY_ENV_PATH=$(poetry env info --path)
+if [[ -e $POETRY_ENV_PATH ]]; then
+    echo "Poetry environment exists."
+    if ! [[ $(poetry check --lock) ]]; then
+        echo "Poetry environment is not up-to-date, updating..."
+        poetry lock --no-update
+    fi
+else
+    echo "Poetry environment doesn't exist. Installing..."
+    poetry install -q
+fi
 . $POETRY_ENV_PATH/bin/activate
-#! Check if the appropriate cuda version is in then PATH
-if ! [[ $PATH == *"cuda-12.1"* ]] && ! [[ $LD_LIBRARY_PATH == *"cuda-12.1"* ]]; then
-    echo "CUDA 12.1 not in PATH or LD_LIBRARY_PATH. Exiting..."
-    echo "PATH=$PATH"
-    echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+# Adding CUDA paths to environment variables
+export PATH=/usr/local/cuda-12.1/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-12.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+#! Check the output of `nvcc -V`
+NVCC_OUTPUT=$(nvcc -V)
+if [[ $NVCC_OUTPUT == *"release 12.1"* ]]; then
+    echo "CUDA 12.1 is detected."
+else
+    echo "CUDA 12.1 not detected. Please install CUDA 12.1. Exiting..."
     exit 1
 fi
-#! Check the output of `nvcc -V`
-nvcc -V
 #! Install `flash-attn`
 if ! [[ $(poetry run pip list | grep flash-attn) ]]; then
     echo "Installing flash-attn..."
     poetry run pip install -q flash-attn==2.3.2 --no-build-isolation
+else
+    echo "flash-attn is already installed."
 fi
 #! Install `xentropy-cuda-lib`
 if ! [[ $(poetry run pip list | grep xentropy) ]]; then
     echo "Installing xentropy-cuda-lib..."
     poetry run pip install -q xentropy-cuda-lib@git+https://github.com/HazyResearch/flash-attention.git@v2.3.2#subdirectory=csrc/xentropy
+else
+    echo "xentropy-cuda-lib is already installed."
 fi
+#! Final message
+echo "Environment is ready."
