54d858e9d92d719f5585ccb20ca3e2638b1820f2
relogu <lollonasi97@gmail.com>
2024-01-06 17:29:51 +0000

    added flops

--- llm_slurm/set_llm_config.sh
+++ llm_slurm/set_llm_config.sh
@@ -24,7 +24,7 @@ if [[ $GPU_TYPE == *'A40'* ]]; then
     LLM_CONFIG_MPT_70B="llm_config=mpt-70b llm_config.device_train_microbatch_size=1 llm_config.device_eval_batch_size=1 llm_config.model.attn_config.attn_impl=torch" # CANNOT DO IT
 elif [[ $GPU_TYPE == *'A100'* ]]; then
     echo "Assuming we are running on A100-SXM4-80GB-equipped machines (CSD3)."
-    FLOP_COUNT="" # Already hardcoded in the MosaicML's callback
+    FLOP_COUNT="llm_config.callbacks.speed_monitor.gpu_flops_available=312e12" # Already hardcoded in the MosaicML's callback, but if passed, we avoid a very bad mistake of them
     #! NOTE: We didn't investigate the performance at inference
     LLM_CONFIG_MPT_SMALL_CPU="llm_config=mpt-small-cpu llm_config.model.init_device=meta llm_config.model.loss_fn=fused_crossentropy llm_config.model.attn_config.attn_impl=triton llm_config.precision=amp_bf16 llm_config.device_train_microbatch_size=512 llm_config.eval_subset_num_batches=-1 llm_config.device_eval_batch_size=1523" # The model is so small that the batch sizes really don't matter
     LLM_CONFIG_MPT_16M="llm_config=mpt-16m llm_config.device_train_microbatch_size=82 llm_config.device_eval_batch_size=400" # This has issues (we can't scale up the microbatch size even though it seems possible) -> https://github.com/Dao-AILab/flash-attention/issues/483
