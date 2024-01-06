6096b3d5b12dc79aa26855711559290ad043156b
relogu <lollonasi97@gmail.com>
2024-01-06 16:19:23 +0000

    bug fixed and verbosity

--- llm_slurm/install_hpc_env.sh
+++ llm_slurm/install_hpc_env.sh
@@ -20,26 +20,55 @@ module load vgl/2.5.1/64
 nvcc -V
 #! Entering the project folder
 cd $HOME/projects/pollen_worker
-#! Install Poetry environment
-if ! [[ $(poetry check --lock) ]]; then
-    echo "Installing Poetry environment..."
-    poetry install -q
+#! Activate Poetry environment
+POETRY_ENV_PATH=$(poetry env info --path)
+if [[ -e $POETRY_ENV_PATH ]]; then
+    echo "Poetry environment exists."
+    if ! [[ $(poetry check --lock) ]]; then
+        echo "Poetry environment is not up-to-date, updating..."
+        poetry lock --no-update
+    fi
 else
-    echo "Poetry environment already installed."
+    echo "Poetry environment doesn't exist. Installing..."
+    poetry install -q
+fi
+. $POETRY_ENV_PATH/bin/activate
+# Adding CUDA paths to environment variables
+export PATH=/usr/local/cuda-12.1/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-12.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+#! Check the output of `nvcc -V`
+NVCC_OUTPUT=$(nvcc -V)
+if [[ $NVCC_OUTPUT == *"release 12.1"* ]]; then
+    echo "CUDA 12.1 is detected."
+else
+    echo "CUDA 12.1 not detected. Please install CUDA 12.1. Exiting..."
+    exit 1
 fi
 #! Install `flash-attn`
 if ! [[ $(poetry run pip list | grep flash-attn) ]]; then
     echo "Installing flash-attn..."
     poetry run pip install -q flash-attn==2.3.2 --no-build-isolation
+else
+    echo "flash-attn is already installed."
 fi
 #! Install `xentropy-cuda-lib`
 if ! [[ $(poetry run pip list | grep xentropy) ]]; then
     echo "Installing xentropy-cuda-lib..."
     poetry run pip install -q xentropy-cuda-lib@git+https://github.com/HazyResearch/flash-attention.git@v2.3.2#subdirectory=csrc/xentropy
+else
+    echo "xentropy-cuda-lib is already installed."
 fi
 #! Downgrade python warnings (default in CSD3 is 'debug')
 #! From here: https://docs.python.org/3/using/cmdline.html#envvar-PYTHONWARNINGS
 #! And here: https://docs.python.org/3/library/warnings.html#describing-warning-filters
 export PYTHONWARNINGS="ignore::DeprecationWarning,ignore::ResourceWarning"
 #! Check Python version
-python --version
+PYTHON_OUTPUT=$(python --version)
+if [[ $PYTHON_OUTPUT == *"3.10.13"* ]]; then
+    echo "Python 3.10.13 is detected."
+else
+    echo "Python 3.10.13 not detected. Please install Python 3.10.13. Exiting..."
+    exit 1
+fi
+#! Final message
+echo "Environment is ready."
