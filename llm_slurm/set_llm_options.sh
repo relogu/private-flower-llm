28a1f5f9e2e3117c91de0ecaba241eaaba67a780
relogu <lollonasi97@gmail.com>
2024-01-18 20:18:32 +0000

    add test conf

--- llm_slurm/set_llm_options.sh
+++ llm_slurm/set_llm_options.sh
@@ -3,5 +3,8 @@
 STEPS="512"
 # #! Configuration: no local checkpointing, no cache limit, no autoresume
 # export LLM_OPTIONS="llm_config.save_interval=${STEPS}ba llm_config.console_log_interval=${STEPS}ba llm_config.save_folder=null llm_config.autoresume=false llm_config.train_loader.dataset.cache_limit=null llm_config.eval_loader.dataset.cache_limit=null llm_config.local_steps=${STEPS}ba"
+# #! Test configuration: no local checkpointing, no cache limit, no autoresume, just 10 steps (both train and eval)
+# STEPS="10"
+# export LLM_OPTIONS="llm_config.save_interval=${STEPS}ba llm_config.console_log_interval=${STEPS}ba llm_config.save_folder=null llm_config.autoresume=false llm_config.train_loader.dataset.cache_limit=null llm_config.eval_loader.dataset.cache_limit=null llm_config.local_steps=${STEPS}ba llm_config.eval_subset_num_batches=10"
 #! Configuration: local checkpointing (w/ overwrite), no cache limit, no autoresume
 export LLM_OPTIONS="llm_config.save_interval=${STEPS}ba llm_config.console_log_interval=${STEPS}ba llm_config.save_folder=$SAVE_PATH llm_config.autoresume=false llm_config.save_overwrite=true llm_config.train_loader.dataset.cache_limit=null llm_config.eval_loader.dataset.cache_limit=null llm_config.local_steps=${STEPS}ba"
