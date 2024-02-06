ba69936dba7dac29bc9b90355c463de59d908c11
relogu <lollonasi97@gmail.com>
2024-02-06 21:55:10 +0000

    centralised training bash script

---
+++ llm_slurm/centralised_training.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#! Check if there's an input argument
+if [[ $# -eq 0 ]]; then
+    echo "No input argument supplied."
+    exit 1
+fi
+#! Moving to the project folder
+cd $HOME/projects/pollen_worker
+#! Preparing environment
+if [[ $(hostname) == *'gpu-q'* ]]; then
+    echo "Assuming the script is executing in the CSD3."
+    #! Executing the environment preparation script
+    #! NOTE: Must use "." to execute, "sh" doesn't work
+    . $HOME/projects/pollen_worker/llm_slurm/install_hpc_env.sh
+else
+    echo "Assuming the script is executing NOT in the CSD3."
+    #! Executing the environment preparation script
+    #! NOTE: Must use "." to execute, "sh" doesn't work
+    . $HOME/projects/pollen_worker/llm_slurm/install_env.sh
+fi
+#! Set `LLM_CONFIG` environment variable
+. $HOME/projects/pollen_worker/llm_slurm/set_llm_config.sh $1
+#! Set `DATA_CONFIG` environment variable
+. $HOME/projects/pollen_worker/llm_slurm/set_llm_data_config.sh "full" false false
+#! Saving path
+DATETIME=$(date '+%Y%m%d_%H%M%S')
+export SAVE_PATH="$HOME/projects/pollen_worker/checkpoints/$DATETIME"
+mkdir -p $SAVE_PATH
+#! Set `LLM_OPTIONS` environment variable
+. $HOME/projects/pollen_worker/llm_slurm/set_llm_options.sh
+#! Getting visible GPUs
+N_GPUS=$(nvidia-smi -L | wc -l)
+CUDA_VISIBLE_DEVICES=$(seq -s, 0 $((N_GPUS-1)))
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+#! Launch centralised training script
+NCCL_BLOCKING_WAIT=1 CUDA_LAUNCH_BLOCKING=1 HYDRA_FULL_ERROR=1 RUN_UUID=$(uuidgen) poetry run composer --world_size $N_GPUS --node_rank 0 --master_addr 127.0.0.1 $HOME/projects/pollen_worker/pollen_worker/centralised_train.py $LLM_CONFIG $LLM_OPTIONS $DATA_CONFIG $POLLEN_CONFIG is_test=false hydra/job_logging=none hydra/hydra_logging=none 2>&1 | tee $SAVE_PATH/centralised_train.log &
+#! Keep the pid and wait for it 
+BACK_PID=$!
+wait $BACK_PID
