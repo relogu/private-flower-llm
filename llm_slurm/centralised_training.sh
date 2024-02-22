f4d10a1de5493f121ab4b3b1f1fdcc952427981f
relogu <lollonasi97@gmail.com>
2024-02-22 22:17:55 +0000

    adding duration parameter

--- llm_slurm/centralised_training.sh
+++ llm_slurm/centralised_training.sh
@@ -33,9 +33,11 @@ mkdir -p $POLLEN_SAVE_PATH
 N_GPUS=$(nvidia-smi -L | wc -l)
 CUDA_VISIBLE_DEVICES=$(seq -s, 0 $((N_GPUS-1)))
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+#! Number of steps
+DURATION=$2
 #! Launch centralised training script
 #! NOTE: Adding `NCCL_BLOCKING_WAIT=1` breaks the optimizer's checkpointing. We don't know why yet.
-CUDA_LAUNCH_BLOCKING=1 HYDRA_FULL_ERROR=1 RUN_UUID=$(uuidgen) poetry run composer --world_size $N_GPUS --node_rank 0 --master_addr 127.0.0.1 $HOME/projects/pollen_worker/pollen_worker/centralised_train.py $LLM_CONFIG $LLM_OPTIONS $DATA_CONFIG run_uuid=centralised-$1-$DATETIME is_test=false hydra/job_logging=none hydra/hydra_logging=none 2>&1 | tee $POLLEN_SAVE_PATH/centralised_train.log &
+CUDA_LAUNCH_BLOCKING=1 HYDRA_FULL_ERROR=1 RUN_UUID=$(uuidgen) poetry run composer --world_size $N_GPUS --node_rank 0 --master_addr 127.0.0.1 $HOME/projects/pollen_worker/pollen_worker/centralised_train.py $LLM_CONFIG $LLM_OPTIONS $DATA_CONFIG llm_config.max_duration=$DURATION llm_config.scheduler.t_max=$DURATION run_uuid=centralised-$1-$DATETIME is_test=false hydra/job_logging=none hydra/hydra_logging=none 2>&1 | tee $POLLEN_SAVE_PATH/centralised_train.log &
 #! Keep the pid and wait for it 
 BACK_PID=$!
 wait $BACK_PID
