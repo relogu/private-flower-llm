c6988b0e4b99e1cb39f2960fa4b72ae90503b646
relogu <lollonasi97@gmail.com>
2024-02-23 23:15:00 +0000

    final LLM_OPTIONS

--- llm_slurm/set_llm_options.sh
+++ llm_slurm/set_llm_options.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 #! Set the run configuration
 STEPS="500"
-# #! Configuration: no local checkpointing, no cache limit, no autoresume
-# export LLM_OPTIONS="llm_config.save_interval=${STEPS}ba llm_config.console_log_interval=${STEPS}ba llm_config.save_folder=null llm_config.autoresume=false llm_config.train_loader.dataset.cache_limit=null llm_config.eval_loader.dataset.cache_limit=null llm_config.local_steps=${STEPS}ba"
-# #! Test configuration: no local checkpointing, no cache limit, no autoresume, just 10 steps (both train and eval)
-# STEPS="10"
-# export LLM_OPTIONS="llm_config.save_interval=${STEPS}ba llm_config.console_log_interval=${STEPS}ba llm_config.save_folder=null llm_config.autoresume=false llm_config.train_loader.dataset.cache_limit=null llm_config.eval_loader.dataset.cache_limit=null llm_config.local_steps=${STEPS}ba llm_config.eval_subset_num_batches=10"
-#! Configuration: local checkpointing (w/ overwrite), no cache limit, no autoresume
-export LLM_OPTIONS="llm_config.save_interval=${STEPS}ba llm_config.console_log_interval=${STEPS}ba llm_config.save_folder=$SAVE_PATH llm_config.autoresume=false llm_config.save_overwrite=true llm_config.train_loader.dataset.cache_limit=null llm_config.eval_loader.dataset.cache_limit=null llm_config.local_steps=${STEPS}ba llm_config.save_num_checkpoints_to_keep=0"
-# #! Configuration: local checkpointing (w/ overwrite), no cache limit, no autoresume, limit eval to 10 steps
-# export LLM_OPTIONS="llm_config.save_interval=${STEPS}ba llm_config.console_log_interval=${STEPS}ba llm_config.save_folder=$SAVE_PATH llm_config.autoresume=false llm_config.save_overwrite=true llm_config.train_loader.dataset.cache_limit=null llm_config.eval_loader.dataset.cache_limit=null llm_config.local_steps=${STEPS}ba llm_config.save_num_checkpoints_to_keep=0 llm_config.eval_subset_num_batches=10"
+# #! Test configuration: just 10 steps during training
+# STEPS="2"
+#! Configuration: local checkpointing, one local checkpoint, no callbacks
+export LLM_OPTIONS="llm_config.save_interval=${STEPS}ba llm_config.console_log_interval=${STEPS}ba llm_config.save_folder=$SAVE_PATH llm_config.local_steps=${STEPS}ba llm_config.save_num_checkpoints_to_keep=1 llm_config.callbacks=null"
