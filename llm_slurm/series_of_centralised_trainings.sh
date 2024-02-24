a6a51d766bde05bc255868ab0ee9611a01cd381b
relogu <lollonasi97@gmail.com>
2024-02-24 12:00:28 +0000

    exp centr

---
+++ llm_slurm/series_of_centralised_trainings.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "125M" "4800ba"
+
+. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "125M" "88000ba"
+
+# . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "160M" "24800ba"
+# . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "160M" "10ba"
+
+. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "160M" "88000ba"
+
+. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "1B" "24800ba"
+# . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "1B" "10ba"
+
+. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "1B" "88000ba"
+
+. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "760M" "29000ba"
+# . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "760M" "10ba"
+
+. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "760M" "88000ba"
+
+. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "3B" "51500ba"
+# . $HOME/projects/pollen_worker/llm_slurm/centralised_training  .sh "3B" "10ba"
+
+. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "3B" "88000ba"
