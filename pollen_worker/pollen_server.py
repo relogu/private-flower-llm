5c848780d101dd658d89aa022c9c2f92f539d6dd
relogu <lollonasi97@gmail.com>
2024-02-25 16:52:45 +0000

    prepared for reumption and checkpointing

--- pollen_worker/pollen_server.py
+++ pollen_worker/pollen_server.py
@@ -97,6 +97,7 @@ class PollenServer(Server):
         ignore_failed_rounds: bool = False,
         print_failures: bool = True,
         print_intentional_failures: bool = True,
+        resume: bool = False,
     ) -> None:
         self.start_up_time = timeit.default_timer()
         self._client_manager: PollenClientManager = client_manager
@@ -121,8 +122,6 @@ class PollenServer(Server):
         )
         self.max_workers: Optional[int] = None
         self.nodes_dict: Dict[str, Tuple[ClientProxy, Node]] = {}
-        if saving_path is None:
-            saving_path = Path(os.getcwd())
         self.saving_path = saving_path
         self.clients_training_stats: Optional[pa.Table] = None
         self.history = history
@@ -133,6 +132,7 @@ class PollenServer(Server):
         self.ignore_failed_rounds = ignore_failed_rounds
         self.print_failures = print_failures
         self.print_intentional_failures = print_intentional_failures
+        self.resume = resume
 
     def set_max_workers(self, max_workers: Optional[int]) -> None:
         """Set the max_workers used by ThreadPoolExecutor."""
@@ -159,20 +159,29 @@ class PollenServer(Server):
         log(INFO, "Waiting for at least one node to connect")
         self._client_manager.wait_for_node_managers(self.num_nodes)
 
-        # Initialize parameters
-        log(INFO, "Initializing global parameters")
-        self.parameters = self._get_initial_parameters(timeout=timeout)
-        log(INFO, "Evaluating initial parameters")
-        res = self.strategy.evaluate(0, parameters=self.parameters)
-        if res is not None:
-            log(
-                INFO,
-                "initial parameters (loss, other metrics): %s, %s",
-                res[0],
-                res[1],
-            )
-            history.add_loss_centralized(server_round=0, loss=res[0])
-            history.add_metrics_centralized(server_round=0, metrics=res[1])
+        # TODO: Resume experiment if asked to
+        time_offset = .0
+        start_round = 1
+        if self.resume:
+            # Download the server state from the S3 bucket
+            # Set the downloaded state to the server
+            pass
+        else:
+            # Initialize parameters
+            log(INFO, "Initializing global parameters")
+            self.parameters = self._get_initial_parameters(timeout=timeout)
+            log(INFO, "Evaluating initial parameters")
+            res = self.strategy.evaluate(0, parameters=self.parameters)
+            if res is not None:
+                log(
+                    INFO,
+                    "initial parameters (loss, other metrics): %s, %s",
+                    res[0],
+                    res[1],
+                )
+                history.add_loss_centralized(server_round=0, loss=res[0])
+                history.add_metrics_centralized(server_round=0, metrics=res[1])
+            # TODO: Save checkpoint if asked to
 
         # NOTE: Register VirtualClients to the PollenClientManager
         self._client_manager.clients = {
@@ -188,7 +197,7 @@ class PollenServer(Server):
         # Run federated learning for num_rounds
         log(INFO, "FL starting")
         start_time = timeit.default_timer()
-        for current_round in range(1, num_rounds + 1):
+        for current_round in range(start_round, num_rounds + 1):
             # Check for changes in connected NodeManagers
             self.check_node_managers()
 
@@ -235,16 +244,11 @@ class PollenServer(Server):
                         server_round=current_round, metrics=evaluate_metrics_fed
                     )
 
-        # Save the statistics to a parquet file
-        if self.clients_training_stats is not None:
-            pq.write_table(
-                self.clients_training_stats,
-                str(self.saving_path / "clients_training_stats.parquet"),
-            )
+            # TODO: Save checkpoint if asked to
 
         # Bookkeeping
         end_time = timeit.default_timer()
-        elapsed = end_time - start_time
+        elapsed = end_time - start_time + time_offset
         log(INFO, "FL finished in %s", elapsed)
         return history
 
