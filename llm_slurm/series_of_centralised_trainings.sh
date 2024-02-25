3bfdc7b34feead63aa62fa06bbd5e3e48e0a8af0
relogu <lollonasi97@gmail.com>
2024-02-25 14:35:09 +0000

    exp

--- llm_slurm/series_of_centralised_trainings.sh
+++ llm_slurm/series_of_centralised_trainings.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-unset SAVE_PATH
-unset RUN_UUID
-export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=100ba"
-. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "75M" "4800ba"
-
-unset SAVE_PATH
-unset RUN_UUID
-export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=0ba"
+export SAVE_PATH="s3://checkpoints/centralised-75M-20240225_003829"
+export RUN_UUID="centralised-75M-20240225_003829"
+export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=100ba wandb.setup.allow_val_change=true wandb.setup.id=centralised-75M-20240225_003829 llm_config.load_path=s3://checkpoints/centralised-75M-20240225_003829/ep0-ba4800-rank0.pt"
+#! Note: The first experiment is run with 4800ba, so we are just extending it for another 83200ba
 . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "75M" "88000ba"
 
-#! Skipping for now
+#! This blows up
+# export SAVE_PATH="s3://checkpoints/centralised-75M-20240225_010800"
+# export RUN_UUID="centralised-75M-20240225_010800"
+# export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=0ba wandb.setup.allow_val_change=true wandb.setup.id=centralised-75M-20240225_010800 llm_config.load_path=s3://checkpoints/centralised-75M-20240225_010800/ep0-ba15500-rank0.pt"
+# . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "75M" "88000ba"
+
+#! Skip for now
 # unset SAVE_PATH
 # unset RUN_UUID
 # export EXTERNAL_CONFIGS="llm_config.save_overwrite=true"
 # . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "125M" "4800ba"
 
-#! Skipping for now
+#! Skip for now
 # unset SAVE_PATH
 # unset RUN_UUID
 # export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=0ba"
@@ -30,7 +32,7 @@ export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_w
 
 export SAVE_PATH="s3://checkpoints/centralised-420M-20240224_223822"
 export RUN_UUID="centralised-420M-20240224_223822"
-export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=0ba wandb.setup.allow_val_change=true wandb.setup.id=centralised-420M-20240224_223822 llm_config.load_path=s3://checkpoints/centralised-420M-20240224_223822/ep0-ba5500-rank0.pt"
+export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=0ba wandb.setup.allow_val_change=true wandb.setup.id=centralised-420M-20240224_223822 llm_config.load_path=s3://checkpoints/centralised-420M-20240224_223822/ep0-ba14500-rank0.pt"
 . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "420M" "88000ba"
 
 #! This blows up
@@ -41,36 +43,32 @@ export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_w
 
 export SAVE_PATH="s3://checkpoints/centralised-160M-20240223_112700"
 export RUN_UUID="centralised-160M-20240223_112700"
-export EXTERNAL_CONFIGS="llm_config.save_overwrite=true wandb.setup.allow_val_change=true llm_config.scheduler.t_warmup=0ba wandb.setup.id=rmfap7hs llm_config.load_path=s3://checkpoints/centralised-160M-20240223_112700/ep0-ba8500-rank0.pt"
+export EXTERNAL_CONFIGS="llm_config.save_overwrite=true wandb.setup.allow_val_change=true llm_config.scheduler.t_warmup=0ba wandb.setup.id=rmfap7hs llm_config.load_path=s3://checkpoints/centralised-160M-20240223_112700/ep0-ba16000-rank0.pt"
 . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "160M" "88000ba"
-
-#! It's the same as the one after this but with fewer total steps, so skipping
-# export SAVE_PATH="s3://checkpoints/centralised-1B-20240223_131951"
-# export RUN_UUID="centralised-1B-20240223_131951"
-# export EXTERNAL_CONFIGS="llm_config.save_overwrite=true wandb.setup.allow_val_change=true llm_config.scheduler.t_warmup=100ba wandb.setup.id=qlwnyu8o llm_config.load_path=s3://checkpoints/centralised-1B-20240223_131951/ep0-ba3500-rank0.pt"
-# . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "1B" "24800ba"
 
 export SAVE_PATH="s3://checkpoints/centralised-1B-20240223_223131"
 export RUN_UUID="centralised-1B-20240223_223131"
-export EXTERNAL_CONFIGS="llm_config.save_overwrite=true wandb.setup.allow_val_change=true llm_config.scheduler.t_warmup=100ba wandb.setup.id=87n121y2 llm_config.load_path=s3://checkpoints/centralised-1B-20240223_223131/ep0-ba3500-rank0.pt"
+export EXTERNAL_CONFIGS="llm_config.save_overwrite=true wandb.setup.allow_val_change=true llm_config.scheduler.t_warmup=100ba wandb.setup.id=87n121y2 llm_config.load_path=s3://checkpoints/centralised-1B-20240223_223131/ep0-ba4000-rank0.pt"
 . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "1B" "88000ba"
 
-unset SAVE_PATH
-unset RUN_UUID
-export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=100ba"
+export SAVE_PATH="s3://checkpoints/centralised-760M-20240225_103905"
+export RUN_UUID="centralised-760M-20240225_103905"
+export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=100ba wandb.setup.id=87n121y2 llm_config.load_path=s3://checkpoints/centralised-760M-20240225_103905/ep0-ba2500-rank0.pt"
 . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "760M" "29000ba"
 
-unset SAVE_PATH
-unset RUN_UUID
-export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=0ba"
+export SAVE_PATH="s3://checkpoints/centralised-760M-20240225_125527"
+export RUN_UUID="centralised-760M-20240225_125527"
+export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=0ba wandb.setup.id=87n121y2 llm_config.load_path=s3://checkpoints/centralised-760M-20240225_125527/ep0-ba2500-rank0.pt"
 . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "760M" "88000ba"
 
-unset SAVE_PATH
-unset RUN_UUID
-export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=100ba"
-. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "3B" "51500ba"
+#! Skip for now
+# unset SAVE_PATH
+# unset RUN_UUID
+# export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=100ba"
+# . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "3B" "51500ba"
 
-unset SAVE_PATH
-unset RUN_UUID
-export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=0ba"
-. $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "3B" "88000ba"
+#! Skip for now
+# unset SAVE_PATH
+# unset RUN_UUID
+# export EXTERNAL_CONFIGS="llm_config.save_overwrite=true llm_config.scheduler.t_warmup=0ba"
+# . $HOME/projects/pollen_worker/llm_slurm/centralised_training.sh "3B" "88000ba"
