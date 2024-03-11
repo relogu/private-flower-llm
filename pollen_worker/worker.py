e565deb8d89a94b7a3f75ab620ff9904ae99a49d
Iacob Alexandru-Andrei <iacobalexandruandrei@gmail.com>
2024-03-11 20:41:57 +0000

    Extend worker to handle lists of clients

--- pollen_worker/worker.py
+++ pollen_worker/worker.py
@@ -15,7 +15,6 @@ import multiprocess as mp
 import pynvml
 import torch
 import transformers
-from flwr.client import NumPyClient
 from flwr.common import NDArrays, Scalar
 from flwr.common.logger import log
 from multiprocess import set_start_method
@@ -27,6 +26,10 @@ from pollen_worker.pollen_utils import (
     write_to_fit_result_shm,
 )
 from pollen_worker.utils import partially_aggregate_with_metrics
+from pollen_worker.datasets.federated_dataset import FederatedDataset
+from torch.utils.data import Dataset, DataLoader
+
+from pollen_worker.virtual_client import VirtualClient
 
 pickle.Pickler = cloudpickle.Pickler  # type: ignore[misc]
 transformers.logging.set_verbosity_error()
@@ -38,128 +41,151 @@ class Worker(mp.Process):
 
     def __init__(
         self,
-        client_fn: Callable[[int], NumPyClient],
+        client_fn: Callable[[int], VirtualClient],
         device: str,
         worker_id: str,
         task_queue: QueueType,
         result_queue: QueueType,
         run_uuid: str,
         concurrency: int,
+        dataset_generator: Callable[[int], Dataset],
+        client_prefetch_num_workers: int = 1,
+        client_prefetch_factor: int = 1,
     ) -> None:
         super().__init__()
         self.worker_id = worker_id
         self.device = device
-        self.client_fn: Callable[[int], NumPyClient] = client_fn
+        self.client_fn: Callable[[int], VirtualClient] = client_fn
         self.task_queue = task_queue
         self.result_queue = result_queue
         self.run_uuid = run_uuid
         self.current_round: int = 0
         self.concurrency = concurrency
+        self.federated_dataset = FederatedDataset(
+            dataset_generator=dataset_generator, list_of_clients=[]
+        )
+        self.client_prefetch_num_workers = client_prefetch_num_workers
+        self.client_prefetch_factor = client_prefetch_factor
 
-    def process_task(self, client_id: int) -> None:
+    def process_task(self, client_ids: list[int]) -> None:
         """Process the received task."""
         # Take the timestamp before training a single client
         start_time = time.time_ns()
         # Loads a dict from the shared memory buffer
         config = pickle.loads(self.config_shm.buf)
         config["device"] = self.device
+        self.federated_dataset.list_of_clients = client_ids
 
-        # Load client
-        tmp_client = self.client_fn(client_id)
-
-        done = False
-        fit_trained_weights: NDArrays | None = None
-        fit_num_samples: int | None = None
-        train_metrics: dict[str, Scalar] | None = None
-        while not done:
-            try:
-                # Call fit on shared parameters
-                fit_trained_weights, fit_num_samples, train_metrics = tmp_client.fit(
-                    self.round_params, config
-                )
-                done = True
-            except Exception as e:
-                log(
-                    ERROR,
-                    "Worker %s failed in training client %s with exception %s."
-                    " Retrying...",
-                    self.worker_id,
-                    client_id,
-                    e,
-                )
-        if (
-            fit_trained_weights is None
-            or fit_num_samples is None
-            or train_metrics is None
+        for client_id, tmp_client_dataset in zip(
+            client_ids,
+            DataLoader(
+                self.federated_dataset,
+                batch_size=1,
+                shuffle=False,
+                collate_fn=lambda x: x[0],
+                num_workers=self.client_prefetch_num_workers,
+                prefetch_factor=self.client_prefetch_factor,
+            ),
+            strict=True,
         ):
-            raise ValueError(
-                f"Worker {self.worker_id} failed in training client {client_id}."
-                " fit_trained_weights, fit_num_samples, or train_metrics is None."
-            )
-        # If new round, then copy result to shared memory directly
-        if config["server_round"] > self.current_round:
-            self.current_round = config["server_round"]
-            write_to_fit_result_shm(
-                self.worker_params,
-                self.worker_num_samples,
-                self.worker_train_loss,
-                self.worker_train_acc,
-                fit_trained_weights,
-                fit_num_samples,
-                float(train_metrics["train_loss"]),
-                float(train_metrics["accuracy"]),
-            )
-        # Partially aggregating fit results
-        else:
-            (
-                tmp_part_agg_params,
-                tmp_part_agg_num_samples,
-                tmp_part_agg_loss,
-                tmp_part_agg_acc,
-            ) = partially_aggregate_with_metrics(
-                (
+            # Load client
+            tmp_client = self.client_fn(client_id)
+            tmp_client.client_dataset = tmp_client_dataset
+
+            done = False
+            fit_trained_weights: NDArrays | None = None
+            fit_num_samples: int | None = None
+            train_metrics: dict[str, Scalar] | None = None
+            while not done:
+                try:
+                    # Call fit on shared parameters
+                    fit_trained_weights, fit_num_samples, train_metrics = (
+                        tmp_client.fit(self.round_params, config)
+                    )
+                    done = True
+                except Exception as e:
+                    log(
+                        ERROR,
+                        "Worker %s failed in training client %s with exception %s."
+                        " Retrying...",
+                        self.worker_id,
+                        client_id,
+                        e,
+                    )
+            if (
+                fit_trained_weights is None
+                or fit_num_samples is None
+                or train_metrics is None
+            ):
+                raise ValueError(
+                    f"Worker {self.worker_id} failed in training client {client_id}."
+                    " fit_trained_weights, fit_num_samples, or train_metrics is None."
+                )
+            # If new round, then copy result to shared memory directly
+            if config["server_round"] > self.current_round:
+                self.current_round = config["server_round"]
+                write_to_fit_result_shm(
                     self.worker_params,
-                    self.worker_num_samples[0],
-                    self.worker_train_loss[0],
-                    self.worker_train_acc[0],
-                ),
-                (
+                    self.worker_num_samples,
+                    self.worker_train_loss,
+                    self.worker_train_acc,
                     fit_trained_weights,
                     fit_num_samples,
                     float(train_metrics["train_loss"]),
                     float(train_metrics["accuracy"]),
-                ),
-            )
-            write_to_fit_result_shm(
-                self.worker_params,
-                self.worker_num_samples,
-                self.worker_train_loss,
-                self.worker_train_acc,
-                tmp_part_agg_params,
-                tmp_part_agg_num_samples,
-                tmp_part_agg_loss,
-                tmp_part_agg_acc,
-            )
-        # Take the timestamp after the task is done
-        end_time = time.time_ns()
-        self.result_queue.put([int(client_id), start_time, end_time, self.device])
-        # NOTE: PyTorch's memory management works bad with multiprocessing. In our case,
-        # it might happen that each process eagerly allocates more MBs of memory on the
-        # same VRAM at the same w/o cleaning the cache because each of them thinks that
-        # it is the only one using the GPU. We need to clean the cache manually to
-        # prevent this, i.e. call `torch.cuda.empty_cache()`. When to call it is a
-        # trade-off between performance and memory usage because cleaning the cache
-        # is time expensive (for Reddit it costs ~15 seconds, quick took ~333s slow
-        # took ~348s in 10 rounds, 100 clients/round, 1 A40). We call it after each
-        # client, but it might be better to call it after each round.
-        for dev_id in range(pynvml.nvmlDeviceGetCount()):
-            handle = pynvml.nvmlDeviceGetHandleByIndex(dev_id)
-            for proc in pynvml.nvmlDeviceGetComputeRunningProcesses(handle):
-                if os.getpid() == proc.pid:
-                    mem = pynvml.nvmlDeviceGetMemoryInfo(handle)
-                    if proc.usedGpuMemory > mem.total / self.concurrency:
-                        torch.cuda.empty_cache()
-                        gc.collect()
+                )
+            # Partially aggregating fit results
+            else:
+                (
+                    tmp_part_agg_params,
+                    tmp_part_agg_num_samples,
+                    tmp_part_agg_loss,
+                    tmp_part_agg_acc,
+                ) = partially_aggregate_with_metrics(
+                    (
+                        self.worker_params,
+                        self.worker_num_samples[0],
+                        self.worker_train_loss[0],
+                        self.worker_train_acc[0],
+                    ),
+                    (
+                        fit_trained_weights,
+                        fit_num_samples,
+                        float(train_metrics["train_loss"]),
+                        float(train_metrics["accuracy"]),
+                    ),
+                )
+                write_to_fit_result_shm(
+                    self.worker_params,
+                    self.worker_num_samples,
+                    self.worker_train_loss,
+                    self.worker_train_acc,
+                    tmp_part_agg_params,
+                    tmp_part_agg_num_samples,
+                    tmp_part_agg_loss,
+                    tmp_part_agg_acc,
+                )
+            # Take the timestamp after the task is done
+            end_time = time.time_ns()
+            self.result_queue.put([int(client_id), start_time, end_time, self.device])
+            # NOTE: PyTorch's memory management works bad with multiprocessing. In our
+            # case, it might happen that each process eagerly allocates more MBs of
+            # memory on the same VRAM at the same w/o cleaning the cache because each
+            # of them thinks that it is the only one using the GPU. We need to clean
+            # the cache manually to prevent this, i.e. call `torch.cuda.empty_cache()`.
+            # When to call it is a trade-off between performance and memory usage
+            # because cleaning the cache is time expensive (for Reddit it costs ~15
+            # seconds, quick took ~333s slow took ~348s in 10 rounds, 100 clients/round,
+            # 1 A40). We call it after each client, but it might be better to call it
+            # after each round.
+            for dev_id in range(pynvml.nvmlDeviceGetCount()):
+                handle = pynvml.nvmlDeviceGetHandleByIndex(dev_id)
+                for proc in pynvml.nvmlDeviceGetComputeRunningProcesses(handle):
+                    if os.getpid() == proc.pid:
+                        mem = pynvml.nvmlDeviceGetMemoryInfo(handle)
+                        if proc.usedGpuMemory > mem.total / self.concurrency:
+                            torch.cuda.empty_cache()
+                            gc.collect()
 
     def run(self) -> None:
         """Start the process."""
@@ -198,7 +224,7 @@ class Worker(mp.Process):
         # NOTE: This is for controlling the GPU memory allocation
         pynvml.nvmlInit()
         # Task loop
-        task: int
+        task: list[int]
         for task in iter(self.task_queue.get, None):
             self.process_task(task)
         # Put the closing task's results in the result queue
