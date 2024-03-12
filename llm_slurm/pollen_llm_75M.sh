ef0f9dda1899362c2b232b74afee9067a78e0c62
relogu <lollonasi97@gmail.com>
2024-03-12 18:17:09 +0000

    local exp

--- llm_slurm/pollen_llm_75M.sh
+++ llm_slurm/pollen_llm_75M.sh
@@ -38,10 +38,9 @@ echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 #! AWS S3 object store settings
 aws_access_key_id=$(grep 'aws_access_key_id' ~/.aws/credentials | awk -F' = ' '{print $2}')
 aws_secret_access_key=$(grep 'aws_secret_access_key' ~/.aws/credentials | awk -F' = ' '{print $2}')
-MINIO_COMM_STACK_OPTIONS="use_minio_comm=true minio.minio_client.endpoint=$S3_ENDPOINT_URL minio.minio_client.access_key=$aws_access_key_id minio.minio_client.secret_key=$aws_secret_access_key minio.minio_state.bucket_name=checkpoints"
+MINIO_COMM_STACK_OPTIONS="use_minio_comm=false minio.minio_client.endpoint=$S3_ENDPOINT_URL minio.minio_client.access_key=$aws_access_key_id minio.minio_client.secret_key=$aws_secret_access_key minio.minio_state.bucket_name=checkpoints"
 #! Set Pollen and FL config
-POLLEN_CONFIG="pollen.server_address='[::]:50744' run_uuid=$RUN_UUID pollen.refresh_period=20 fl.n_rounds=176 pollen.checkpoint=true llm_config.scheduler.t_max=88000ba llm_config.scheduler.t_warmup=1000ba llm_config.scheduler.alpha_f=0.00001 llm_config.optimizer.lr=4.0e-4 llm_config.save_overwrite=true pollen.n_nodes=4 pollen.resume_round=-1 pollen.fit_collaborative=true"
-# POLLEN_CONFIG="$POLLEN_CONFIG pretrained_model_path=$HOME/projects/pollen_worker/cen_1B_checkpoint.bin"
+POLLEN_CONFIG="pollen.server_address='localhost:50744' run_uuid=$RUN_UUID pollen.refresh_period=20 fl.n_rounds=176 pollen.checkpoint=true llm_config.scheduler.t_max=88000ba llm_config.scheduler.t_warmup=1000ba llm_config.scheduler.alpha_f=0.00001 llm_config.optimizer.lr=4.0e-4 llm_config.save_overwrite=true pollen.n_nodes=1 pollen.resume_round=-1"
 #! Launch ServerWithPollen
 GRPC_VERBOSITY=debug HYDRA_FULL_ERROR=1 poetry run python -m pollen_worker.launch_pollen_server $LLM_CONFIG $LLM_OPTIONS $DATA_CONFIG $POLLEN_CONFIG pollen.saving_path=$SAVE_PATH $MINIO_COMM_STACK_OPTIONS 2>&1 | tee $POLLEN_SAVE_PATH/server.log &
 #! Wait for 30 seconds. This is needed because of how the client connection behaves.
