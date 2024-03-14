eea06d49bb5a5cee17569288ba63a31eadeca5cf
relogu <lollonasi97@gmail.com>
2024-03-14 23:15:56 +0000

    adding LR monitor callback for clients

--- llm_slurm/set_llm_options.sh
+++ llm_slurm/set_llm_options.sh
@@ -5,7 +5,7 @@ STEPS="500"
 # STEPS="2"
 #! Configuration: local checkpointing, one local checkpoint, no callbacks
 export LLM_OPTIONS="llm_config.save_interval=${STEPS}ba llm_config.console_log_interval=${STEPS}ba llm_config.save_folder=$SAVE_PATH llm_config.local_steps=${STEPS}ba llm_config.save_num_checkpoints_to_keep=1"
-#! Configuration: no callbacks
-export LLM_OPTIONS="$LLM_OPTIONS llm_config.callbacks=null"
+#! Configuration: remove default callbacks except for the LR monitor
+export LLM_OPTIONS="$LLM_OPTIONS ~llm_config.callbacks.speed_monitor ~llm_config.callbacks.memory_monitor ~llm_config.callbacks.runtime_estimator"
 #! Configuration: reduce number of eval batches
 # export LLM_OPTIONS="$LLM_OPTIONS llm_config.eval_subset_num_batches=2"
