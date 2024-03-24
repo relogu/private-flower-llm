7a86c72e432d9d3f085820a3d80a919e6ef38865
relogu <lollonasi97@gmail.com>
2024-03-24 21:31:02 +0000

    exp

--- llm_slurm/pollen_llm_1B.sh
+++ llm_slurm/pollen_llm_1B.sh
@@ -39,7 +39,7 @@ echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 MINIO_COMM_STACK_OPTIONS="use_s3_comm=true s3_comm_config.bucket_name=checkpoints"
 #! Set Pollen and FL config
 POLLEN_CONFIG="pollen.server_address='[::]:50758' run_uuid=$RUN_UUID pollen.refresh_period=20 fl.n_rounds=176"
-POLLEN_CONFIG="$POLLEN_CONFIG pollen.checkpoint=true llm_config.save_overwrite=true pollen.n_nodes=1 pollen.resume_round=-1 pollen.fit_collaborative=false pollen.restore_run_uuid=null"
+POLLEN_CONFIG="$POLLEN_CONFIG pollen.checkpoint=true llm_config.save_overwrite=true pollen.n_nodes=1 pollen.resume_round=6 pollen.fit_collaborative=true pollen.restore_run_uuid=null"
 POLLEN_CONFIG="$POLLEN_CONFIG llm_config.scheduler.t_max=24800ba llm_config.scheduler.t_warmup=100ba llm_config.scheduler.alpha_f=0.1 llm_config.optimizer.lr=2.0e-4"
 #! Launch ServerWithPollen
 GRPC_VERBOSITY=debug HYDRA_FULL_ERROR=1 poetry run python -m pollen_worker.launch_pollen_server $LLM_CONFIG $LLM_OPTIONS $DATA_CONFIG $POLLEN_CONFIG pollen.saving_path=$SAVE_PATH $MINIO_COMM_STACK_OPTIONS 2>&1 | tee $POLLEN_SAVE_PATH/server.log &
--- llm_slurm/series_of_federated_trainings.sh
+++ llm_slurm/series_of_federated_trainings.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 unset RUN_UUID
+export RUN_UUID="fed-1B-20240322_221900-fix-nesto"
 unset SAVE_PATH
 . $HOME/projects/pollen_worker/llm_slurm/pollen_llm_1B.sh
+
+# unset RUN_UUID
+# export RUN_UUID="fed-75M-reset-20240323_183000-fix-nesto"
+# unset SAVE_PATH
+# . $HOME/projects/pollen_worker/llm_slurm/pollen_llm_75M.sh
+
+# unset RUN_UUID
+# export RUN_UUID="fed-125M-reset-20240323_213000-fix-nesto"
+# unset SAVE_PATH
+# . $HOME/projects/pollen_worker/llm_slurm/pollen_llm_125M.sh
