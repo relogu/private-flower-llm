cb529fb631a6a1c82f0bc73182f6a3733857e6f9
relogu <lollonasi97@gmail.com>
2024-03-25 22:42:20 +0000

    use all the callbacks

--- llm_slurm/set_llm_options.sh
+++ llm_slurm/set_llm_options.sh
@@ -6,6 +6,6 @@ STEPS="500"
 #! Configuration: local checkpointing, one local checkpoint, no callbacks
 export LLM_OPTIONS="llm_config.save_interval=${STEPS}ba llm_config.console_log_interval=${STEPS}ba llm_config.save_folder=$SAVE_PATH llm_config.local_steps=${STEPS}ba llm_config.save_num_checkpoints_to_keep=1"
 #! Configuration: remove default callbacks except for the LR monitor
-export LLM_OPTIONS="$LLM_OPTIONS ~llm_config.callbacks.speed_monitor ~llm_config.callbacks.memory_monitor ~llm_config.callbacks.runtime_estimator"
+# export LLM_OPTIONS="$LLM_OPTIONS ~llm_config.callbacks.speed_monitor ~llm_config.callbacks.memory_monitor ~llm_config.callbacks.runtime_estimator"
 #! Configuration: reduce number of eval batches
 # export LLM_OPTIONS="$LLM_OPTIONS llm_config.eval_subset_num_batches=2"
