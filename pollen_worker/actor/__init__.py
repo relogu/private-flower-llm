3ea9bd383fb4ab51731c96469880ef963f25f94e
Iacob Alexandru-Andrei <iacobalexandruandrei@gmail.com>
2024-04-04 11:18:48 +0100

    Add actor for easy ray integration

---
+++ pollen_worker/actor/__init__.py
@@ -0,0 +1,1 @@
+"""A ray actor interface for pollen."""
---
+++ pollen_worker/actor/actor.py
@@ -0,0 +1,107 @@
+"""A ray actor pool node-manager interface."""
+
+from typing import cast
+
+from flwr.simulation.ray_transport.ray_actor import (
+    VirtualClientEngineActor,
+    ClientException,
+    ClientRes,
+)
+import traceback
+
+import ray
+
+from flwr.client import Client, ClientFn
+from flwr.common.context import Context
+from flwr.simulation.ray_transport.utils import check_clientfn_returns_client
+
+
+import pickle
+from collections.abc import Callable
+from logging import WARNING
+
+import cloudpickle
+import transformers
+from flwr.common import (
+    NDArrays,
+)
+from flwr.common.logger import log
+from multiprocess import set_start_method  # type: ignore[reportAttributeAccessIssue]
+from omegaconf import DictConfig
+
+from pollen_worker.clients.virtual_llm_client import VirtualLLMClient
+from pollen_worker.node_manager.node_manager import NodeManager
+
+transformers.logging.set_verbosity_error()
+set_start_method("spawn", force=True)
+pickle.Pickler = cloudpickle.Pickler  # type: ignore[misc]
+
+
+# All possible returns by a client
+# A function to be executed by a client to obtain some results
+JobFn = Callable[[Client], ClientRes]
+
+
+class VirtualClientEngineActorPollen(VirtualClientEngineActor):
+    """Abstract base class for VirtualClientEngine Actors."""
+
+    def __init__(
+        self,
+        client_fn: Callable[[int], VirtualLLMClient],
+        run_uuid: str,
+        parameters: NDArrays,
+        refresh_period: int,
+        use_s3_comm: bool = False,
+        s3_comm_config: DictConfig | None = None,
+        delayed_resource_init: bool = False,
+    ) -> None:
+        super().__init__()
+        self.node_manager = NodeManager(
+            client_fn=client_fn,
+            run_uuid=run_uuid,
+            parameters=parameters,
+            refresh_period=refresh_period,
+            use_s3_comm=use_s3_comm,
+            s3_comm_config=s3_comm_config,
+            delayed_resource_init=delayed_resource_init,
+        )
+
+    def terminate(self) -> None:
+        """Manually terminate Actor object."""
+        log(WARNING, "Manually terminating %s}", self.__class__.__name__)
+        ray.actor.exit_actor()
+
+    def run(
+        self,
+        client_fn: ClientFn,
+        job_fn: JobFn,
+        cid: str,
+        context: Context,
+    ) -> tuple[str, ClientRes, Context]:
+        """Run a client run."""
+        # Execute tasks and return result
+        # return also cid which is needed to ensure results
+        # from the pool are correctly assigned to each ClientProxy
+        try:
+            # Instantiate client (check 'Client' type is returned)
+            client = check_clientfn_returns_client(client_fn(cid))
+            # Inject context
+            client.set_context((context, self.node_manager))  # type: ignore[reportArgumentType,arg-type]
+            # Run client job
+            job_results = job_fn(client)
+            # Retrieve context (potentially updated)
+            updated_context, _ = cast(tuple[Context, NodeManager], client.get_context())  # type: ignore[reportGeneralTypeIssues]
+            client.set_context(updated_context)
+        except Exception as ex:
+            client_trace = traceback.format_exc()
+            message = (
+                "\n\tSomething went wrong when running your client run.\n\tClient "
+                + cid
+                + " crashed when the "
+                + self.__class__.__name__
+                + " was running its run.\n\tException triggered on the client side: "
+                + client_trace,
+            )
+            raise ClientException(str(message)) from ex
+
+        return cid, job_results, updated_context
