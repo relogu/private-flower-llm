dd40d9a72293f81827dc52a3df209dd851427b74
relogu <lollonasi97@gmail.com>
2024-04-15 10:41:35 +0100

    pollen google speech expr -- use heterogeneous job for slurm

--- lorenzo_slurm/slurm_pollen_llb_1+3_google_speech_scale10000.sh
+++ lorenzo_slurm/slurm_pollen_llb_1+3_google_speech_scale10000.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=PG13-bu
+#SBATCH --job-name=PG13-llb-scale10k
 #SBATCH --output=%x-%j.out
 #SBATCH --partition=normal
 #SBATCH --gres=gpu:a40:1
@@ -27,7 +27,7 @@ POETRY_ENV_PATH=$(poetry env info --path)
 
 
 # Set the custom hydra arguments that will be passed to the server and the node manager
-CUSTOM_HYDRA_ARGS="num_nodes=2 run_uuid=$run_uuid task=google_speech task.n_clients_per_round=100 task.num_rounds=100 local_epochs=1 placement_policy=bu flwr_address=$ip:6481"
+CUSTOM_HYDRA_ARGS="num_nodes=2 run_uuid=$run_uuid task=google_speech task.n_clients_per_round=10000 task.num_rounds=100 local_epochs=1 placement_policy=llb flwr_address=$ip:6481"
 
 echo "STARTING POLLEN SERVER at $node_1"
 poetry run python -m pollen_worker.launch_pollen_server $CUSTOM_HYDRA_ARGS hydra/job_logging=none hydra/hydra_logging=none &
--- lorenzo_slurm/slurm_pollen_rr_1+3_google_speech_scale2000.sh
+++ lorenzo_slurm/slurm_pollen_rr_1+3_google_speech_scale2000.sh
@@ -1,47 +1,43 @@
 #!/bin/bash
-#SBATCH --nodelist=mauao,ngongotaha
-#SBATCH --gres=gpu:3
 #SBATCH --job-name=PG13-rr-scale2k
-#SBATCH --cpus-per-task 24
-#SBATCH --ntasks-per-node=1
 #SBATCH --output=%x-%j.out
-#SBATCH --time=07:00:00
-#SBATCH --dependency=afterany:78068
+#SBATCH --partition=normal
+#SBATCH --gres=gpu:a40:1
+#SBATCH -c 10
+#SBATCH hetjob
+#SBATCH --partition=normal
+#SBATCH --gres=gpu:rtx2080:3
+#SBATCH -c 24
 
 #! Need to force the nodes. Otherwise, the nodes might be allocated randomly.
 #! Head node is `mauao`, 128.232.115.0
 node_1="mauao"
 node_2="ngongotaha"
 ip="128.232.115.0"
+echo "The script is being executed in node $(hostname)"
 
 # Get the timestamp and the unique run id
 timestamp=$(date +%Y-%m-%d_%H%M%S)
 run_uuid=$(uuidgen)
-# activate the environment and go to the pollen_worker directory
-cd $HOME/projects/pollen_worker
+# Activate the environment and go to the pollen_worker directory
+# cd $HOME/projects/pollen_worker
 #! Activate Poetry environment
 POETRY_ENV_PATH=$(poetry env info --path)
 . $POETRY_ENV_PATH/bin/activate
 
 
 # Set the custom hydra arguments that will be passed to the server and the node manager
-policy="rr"
-echo "Using policy $policy"
-CUSTOM_HYDRA_ARGS="num_nodes=2 run_uuid=$run_uuid task=google_speech task.n_clients_per_round=2000 task.num_rounds=10 local_epochs=1 placement_policy=$policy flwr_address=$ip:6381"
+CUSTOM_HYDRA_ARGS="num_nodes=2 run_uuid=$run_uuid task=google_speech task.n_clients_per_round=2000 task.num_rounds=100 local_epochs=1 placement_policy=rr flwr_address=$ip:6481"
 
 echo "STARTING POLLEN SERVER at $node_1"
 poetry run python -m pollen_worker.launch_pollen_server $CUSTOM_HYDRA_ARGS hydra/job_logging=none hydra/hydra_logging=none &
 
+sleep 15
+
 echo "STARTING POLLEN NODE MANAGER at $node_1"
-srun --nodes=1 --ntasks=1 -w "$node_1" --gres=gpu:1 -c 24 \
+srun --het-group=0 \
     poetry run python -m pollen_worker.node_manager $CUSTOM_HYDRA_ARGS hydra/job_logging=none hydra/hydra_logging=none &
 
 echo "STARTING POLLEN NODE MANAGER at $node_2"
-srun --nodes=1 --ntasks=1 -w "$node_2" \
-    poetry run python -m pollen_worker.node_manager $CUSTOM_HYDRA_ARGS hydra/job_logging=none hydra/hydra_logging=none
-
-
-# How to use this script? Use what follows for a interactive job
-# srun --nodelist mauao,ngongotaha --cpus-per-task 8 --ntasks-per-node=1 --gres=gpu:1 --partition=interactive bash lorenzo_slurm/slurm_pollen_multinode.sh
-# Use what follows for a batch job
-# sbatch lorenzo_slurm/slurm_pollen_multinode.sh
+srun --het-group=1 \
+    poetry run python -m pollen_worker.node_manager $CUSTOM_HYDRA_ARGS hydra/job_logging=none hydra/hydra_logging=none 
