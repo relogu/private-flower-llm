2a54cda3150b553ff68cf9f9a3722e0c58dd951e
relogu <lollonasi97@gmail.com>
2024-04-22 17:57:25 +0100

    reconcile typing for parameters

--- pollen_worker/strategy/rs_fedyogi.py
+++ pollen_worker/strategy/rs_fedyogi.py
@@ -168,6 +168,7 @@ class FedYogiRSModel(FedYogiReproducibleSampling):
     def __init__(
         self,
         *,
+        initial_parameters: Parameters,
         saving_path: Path | None = None,
         fraction_fit: float = 1.0,
         fraction_evaluate: float = 1.0,
@@ -184,7 +185,6 @@ class FedYogiRSModel(FedYogiReproducibleSampling):
         on_fit_config_fn: Callable[[int], dict[str, Scalar]] | None = None,
         on_evaluate_config_fn: Callable[[int], dict[str, Scalar]] | None = None,
         accept_failures: bool = True,
-        initial_parameters: Parameters | None = None,
         fit_metrics_aggregation_fn: MetricsAggregationFn | None = None,
         evaluate_metrics_aggregation_fn: MetricsAggregationFn | None = None,
         eta: float = 1e-2,
