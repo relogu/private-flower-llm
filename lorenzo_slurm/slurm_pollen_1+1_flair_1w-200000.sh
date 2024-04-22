cec379743ddf7bd06c2639707ee10fbe672de54c
relogu <lollonasi97@gmail.com>
2024-04-22 14:59:49 +0100

    new exp

---
+++ lorenzo_slurm/slurm_pollen_1+1_flair_1w-200000.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#!SBATCH --job-name=hetero-test
+#SBATCH --job-name=PF11-mw-200000
+#SBATCH --output=%x-%j.out
+#SBATCH --partition=normal
+#SBATCH --gres=gpu:a40:1
+#SBATCH -c 10
+#SBATCH hetjob
+#SBATCH --partition=normal
+#SBATCH --gres=gpu:rtx2080:1
+#SBATCH -c 8
+
+#! Need to force the nodes. Otherwise, the nodes might be allocated randomly.
+#! Head node is `mauao`, 128.232.115.0
+node_1="mauao"
+node_2="ngongotaha"
+ip="128.232.115.0"
+echo "The script is being executed in node $(hostname)"
+
+# Get the timestamp and the unique run id
+timestamp=$(date +%Y-%m-%d_%H%M%S)
+# # activate the environment and go to the pollen_worker directory
+# cd $HOME/projects/pollen_worker
+#! Activate Poetry environment
+POETRY_ENV_PATH=$(poetry env info --path)
+. $POETRY_ENV_PATH/bin/activate
+
+
+# Set the custom hydra arguments that will be passed to the server and the node manager
+# for policy in "lb" "bu" "rr"; do
+for policy in "lb"; do
+    run_uuid="PF-13-$policy-mw-200000-$timestamp"
+    echo "Using policy $policy"
+    CUSTOM_HYDRA_ARGS="num_nodes=2 run_uuid=$run_uuid task=flair task.n_clients_per_round=200000 task.num_rounds=10 local_epochs=2 placement_policy=$policy flwr_address=$ip:7384"
+
+    echo "STARTING POLLEN SERVER at $node_1"
+    GRPC_VERBOSITY=debug poetry run python -m pollen_worker.launch_pollen_server $CUSTOM_HYDRA_ARGS hydra/job_logging=none hydra/hydra_logging=none &
+    sleep 15
+    echo "STARTING POLLEN NODEMANAGER at $node_1"
+    GRPC_VERBOSITY=debug srun --het-group=0 \
+        poetry run python -m pollen_worker.node_manager $CUSTOM_HYDRA_ARGS cap_num_workers_per_gpu=1 hydra/job_logging=none hydra/hydra_logging=none &
+    echo "STARTING POLLEN NODEMANAGER at $node_2"
+    GRPC_VERBOSITY=debug srun --het-group=1 \
+        poetry run python -m pollen_worker.node_manager $CUSTOM_HYDRA_ARGS cap_num_workers_per_gpu=5 hydra/job_logging=none hydra/hydra_logging=none 
+done
--- lorenzo_slurm/slurm_pollen_1gpu_cifar10.sh
+++ lorenzo_slurm/slurm_pollen_1gpu_cifar10.sh
@@ -2,14 +2,12 @@
 #SBATCH -c 10
 #SBATCH -w mauao
 #SBATCH --gres=gpu:1
-#SBATCH --job-name=PC1
+#SBATCH --job-name=conc-PC1
 #SBATCH --tasks-per-node=1
 #SBATCH --output=%x-%j.out
-#!SBATCH --dependency=afterany:77858
+#SBATCH -t 04:00:00
+#SBATCH --dependency=afterany:2131
 
-# Get the timestamp and the unique run id
-timestamp=$(date +%Y-%m-%d_%H%M%S)
-run_uuid=$(uuidgen)
 # \activate the environment and go to the pollen_worker directory
 # cd $HOME/projects/pollen_worker
 #! Activate Poetry environment
@@ -18,19 +16,37 @@ POETRY_ENV_PATH=$(poetry env info --path)
 
 # Set the custom hydra arguments that will be passed to the server and the node manager
 # for policy in "lb" "llb" "rr"; do
-for policy in "lb"; do
-    echo "Using policy $policy"
-    CUSTOM_HYDRA_ARGS="num_nodes=1 run_uuid=$run_uuid task=cifar10 placement_policy=$policy flwr_address=127.0.0.1:6481"
+for n_workers in $(seq 1 16); do
+    policy="rr"
+    # Get the timestamp and the unique run id
+    timestamp=$(date +%Y-%m-%d_%H%M%S)
+    run_uuid=conc-est-cifar-1gpu-a40-$n_workers-$timestamp
+    export RUN_UUID=$run_uuid
+    export CONCURRENCY=$n_workers
+    echo "Using policy $policy with $n_workers workers. Run UUID: $run_uuid"
+    CUSTOM_HYDRA_ARGS="num_nodes=1 cap_num_workers_per_gpu=$CONCURRENCY run_uuid=$run_uuid task=cifar10 task.num_rounds=100 placement_policy=$policy flwr_address=127.0.0.1:9481"
 
     # Launch the server.
-    poetry run python -m pollen_worker.launch_pollen_server $CUSTOM_HYDRA_ARGS hydra/job_logging=none hydra/hydra_logging=none  &
+    GRPC_VERBOSITY=debug poetry run python -m pollen_worker.launch_pollen_server $CUSTOM_HYDRA_ARGS hydra/job_logging=none hydra/hydra_logging=none  &
+    sleep 30
 
     # Launch the node manager.
-    poetry run python -m pollen_worker.node_manager $CUSTOM_HYDRA_ARGS hydra/job_logging=none hydra/hydra_logging=none 
-done
+    GRPC_VERBOSITY=debug poetry run python -m pollen_worker.node_manager $CUSTOM_HYDRA_ARGS hydra/job_logging=none hydra/hydra_logging=none &
+    # Retrieve the PID of the last process launched
+    node_manager_pid=$!
 
-# How to use this script? Use what follows for a interactive job
-# srun -w mauao -c 11 --gres=gpu:1 --partition=interactive bash slurm_pollen_singlenode.sh
-# srun -w ngongotaha -c 8 --gres=gpu:1 --partition=interactive bash slurm_pollen_singlenode.sh
-# Use what follows for a batch job
-# sbatch slurm_pollen_singlenode.sh
+    # Lauch GPU monitoring
+    nvidia-smi --query-gpu=index,utilization.gpu,memory.total,memory.used,memory.free,timestamp --format=csv,noheader,nounits --filename=gpu-$RUN_UUID-$CONCURRENCY.csv --loop-ms=100 &
+    # Retrieve the PID of the last process launched
+    gpu_monitoring_pid=$!
+    # Launch CPU, MEMORY, and DISK monitoring
+    ./monitoring.sh $node_manager_pid &
+    # Retrieve the PID of the last process launched
+    monitoring_pid=$!
+    # Wait for the node manager to finish
+    wait $node_manager_pid
+    # Kill the GPU monitoring
+    kill $gpu_monitoring_pid
+    # Kill the CPU, MEMORY, and DISK monitoring
+    kill $monitoring_pid
+done
