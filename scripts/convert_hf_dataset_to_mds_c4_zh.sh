1a8a81bde42db207921f3721127fbea9c81b7833
relogu <lollonasi97@gmail.com>
2024-09-23 08:42:06 +0100

    dataset conversion script

---
+++ scripts/convert_hf_dataset_to_mds_c4_zh.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# shellcheck disable=SC2090,SC2086,SC2089,SC1091
+# Default project path
+PROJECT_PATH="$HOME/projects/flower_llm"
+
+# Parse command-line options
+if ! OPTIONS=$(getopt -o p: --long project_path: -n 'parse-options' -- "$@"); then
+	echo "Error parsing options" >&2
+	exit 1
+fi
+
+eval set -- "$OPTIONS"
+
+while true; do
+	case "$1" in
+	-p | --project_path)
+		PROJECT_PATH="$2"
+		shift 2
+		;;
+	--)
+		shift
+		break
+		;;
+	*)
+		break
+		;;
+	esac
+done
+
+echo "PROJECT_PATH=$PROJECT_PATH"
+#! Moving to the project folder
+cd "$PROJECT_PATH" || exit
+#! Preparing environment
+if [[ $(hostname) == *'gpu-q'* ]]; then
+	echo "Assuming the script is executing in the CSD3."
+	#! Executing the environment preparation script
+	#! NOTE: Must use "." to execute, "sh" doesn't work
+	. "$PROJECT_PATH"/scripts/install_hpc_env.sh
+else
+	echo "Assuming the script is executing NOT in the CSD3."
+	#! Executing the environment preparation script
+	#! NOTE: Must use "." to execute, "sh" doesn't work
+	. "$PROJECT_PATH"/scripts/install_env.sh
+fi
+#! Export the endpoint of the S3 object store, using directly the IP to avoid name resolution issues
+export S3_ENDPOINT_URL='http://128.232.115.0:9000'
+
+#! Execute the command
+poetry run python -m flower_llm.dataset.convert_dataset_hf \
+	--path "allenai/c4" \
+	--name zh \
+	--splits validation train \
+	--tokenizer /nfs-share/ls985/projects/flower_llm/tokenizer_20240921225741_v-50257_l-2048_d-allenai-c4_n-zh_s-train \
+	--num_clients 8 \
+	--num_workers 20
+
+#! Remove the positional arguments
+eval set --
