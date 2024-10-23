5e3f02f1f8bb82160140dab50319ac16786ae0e2
relogu <lollonasi97@gmail.com>
2024-10-23 10:29:42 +0100

    update setting

--- scripts/fake_grad_step1_2cpr.sh
+++ scripts/fake_grad_step1_2cpr.sh
@@ -66,9 +66,9 @@ export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.save_interval=${LOCAL_STEP
 export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS fl.n_total_clients=$CPR"
 export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS fl.fake_gradient_update=true fl.fake_gradient_update_steps=1"
 export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS fl.eval_fl=$EVAL_FREQ"
-# Linear scaling
-export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS ++fl.strategy_kwargs.scaling_fn=linear"
-# Square root scaling
-export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS ++fl.strategy_kwargs.scaling_fn=sqrt"
+# # Linear scaling
+# export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS ++fl.strategy_kwargs.scaling_fn=linear"
+# # Square root scaling
+# export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS ++fl.strategy_kwargs.scaling_fn=sqrt"
 
 bash $HOME/projects/flower_llm/scripts/photon_llm_125M.sh 125M
