c07c0748e70406df19b2a7fa57d60678d0502c8d
relogu <lollonasi97@gmail.com>
2024-10-29 22:11:14 +0000

    single client benchmarks

--- scripts/cen_1b_bench.sh
+++ scripts/cen_1b_bench.sh
@@ -31,7 +31,7 @@ while true; do
 done
 echo "slurm_submit.mauao: PROJECT_PATH=$PROJECT_PATH"
 
-BATCH_SIZE=512
+BATCH_SIZE=64
 TOTAL_STEPS=24800
 WARMUP_STEPS=100
 EVAL_FREQ=100
@@ -46,7 +46,7 @@ export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.optimizer.lr=2.0e-4"
 export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.global_train_batch_size=$BATCH_SIZE"
 export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.device_train_microbatch_size=auto"
 export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.precision=amp_bf16"
-export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.fsdp_config.sharding_strategy=NO_SHARD" # DDP
+export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS ~llm_config.fsdp_config" # DDP
 # export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.fsdp_config.sharding_strategy=FULL_SHARD" # FSDP (full shard)
 # export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.fsdp_config.sharding_strategy=SHARD_GRAD_OP" # FSDP (shard grad op)
 export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS fl.strategy_kwargs.server_learning_rate=1.0"
@@ -59,6 +59,6 @@ export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS ++llm_config.device_eval_microbatch_s
 export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.eval_interval=${TOTAL_STEPS}ba"
 export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS fl.reset_optimizer=false"
 export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.save_interval=${EVAL_FREQ}ba"
-export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.eval_interval=${EVAL_FREQ}ba"
+export EXTERNAL_CONFIGS="$EXTERNAL_CONFIGS llm_config.eval_first=false"
 
 bash $HOME/projects/flower_llm/scripts/centralised_training.sh 1B
