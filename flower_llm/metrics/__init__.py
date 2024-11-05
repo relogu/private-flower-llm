3fc748ce8cd48a34f81560b6435c3ceb265bbb77
Iacob Alexandru-Andrei <iacobalexandruandrei@gmail.com>
2024-11-05 13:35:41 +0000

    Add unigram perplexity

--- flower_llm/metrics/__init__.py
+++ flower_llm/metrics/__init__.py
@@ -1,1 +1,17 @@
 """Metrics to use with MosaicML Composer."""
+
+from llmfoundry.registry import metrics
+from flower_llm.metrics.unigram_normalized_metrics import (
+    UnigramNormalizedLanguageCrossEntropy,
+    UnigramNormalizedLanguagePerplexity,
+)
+
+metrics.register(
+    "unigram_normalized_language_cross_entropy",
+    func=UnigramNormalizedLanguageCrossEntropy,
+)
+
+metrics.register(
+    "unigram_normalized_language_perplexity",
+    func=UnigramNormalizedLanguagePerplexity,
+)
