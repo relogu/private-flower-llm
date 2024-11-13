90fa6c220b53edc4b3261deca4d3c6e8f65e4e05
Iacob Alexandru-Andrei <iacobalexandruandrei@gmail.com>
2024-11-13 00:25:32 +0000

    Update

--- flower_llm/strategy/metrics.py
+++ flower_llm/strategy/metrics.py
@@ -4,7 +4,6 @@ from typing import Any
 from flwr.common import NDArrays
 
 from flower_llm.conf.base_schema import BaseConfig
-from flower_llm.wandb_history import WandbHistory
 
 
 class ServerMetricCallback:
@@ -25,9 +24,9 @@ class ServerMetricCallback:
 class SimpleNoiseScale(ServerMetricCallback):
     """Callback for collecting metrics on the server side."""
 
-    def __init__(self, history: WandbHistory, conf: BaseConfig) -> None:
+    def __init__(self, metrics: dict[str, Any], conf: BaseConfig) -> None:
         """Initialize the ServerMetricCallback object."""
-        self.history = history
+        self.metrics = metrics
         self.summed_grads_squares: list[float] | None = None
         self.b_small = conf.llm_config.global_train_batch_size
         self.b_big = self.b_small * conf.fl.n_clients_per_round
@@ -63,6 +62,4 @@ class SimpleNoiseScale(ServerMetricCallback):
 
         noise_scale = scale / noise
 
-        self.history.add_metrics_centralized(
-            current_round, metrics={"noise_scale": noise_scale}
-        )
+        self.metrics |= {"server/noise_scale": noise_scale}
